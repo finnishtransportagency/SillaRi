fd48dc734f6d1040606d84badfe7a1e4986cdf9b
Jari Piitulainen <jari.piitulainen@cgi.com>
2020-12-31 09:02:40 +0200

    fix logs folder and startapp

--- docker/sillari-backend/startapp.sh
+++ docker/sillari-backend/startapp.sh
@@ -8,8 +8,6 @@ else
     if [ -f "/var/sillari/node2" ]; then
         nodename="node2"
         export nodename
-        nohup tcpdump port 2000 > /logs/node1/tcpdump.txt &
-    nohup tcpdump host 172.30.37.37 > /logs/node1/tcpdump-host.txt &
         java -Xms4092m -Xmx4092m -Druntime.nodename=node2 -Denv=${environment} -Druntime.environment=${environment} -Djava.security.egd=file:/dev/./urandom -Duser.timezone=Europe/Helsinki -XX:-OmitStackTraceInFastThrow -jar /sillari.jar
     else
         nodename="nodex"
