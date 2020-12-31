d6fc50eaf6de0c3c19e416b4f92d045173d9a800
Jari Piitulainen <jari.piitulainen@cgi.com>
2020-12-31 07:17:58 +0200

    cloudformation templates

--- docker/sillari-backend/startapp.sh
+++ docker/sillari-backend/startapp.sh
@@ -3,21 +3,17 @@ cat /var/sillari/hosts >> /etc/hosts
 if [ -f "/var/sillari/node1" ]; then
     nodename="node1"
     export nodename
-    nohup tcpdump port 2000 > /logs/node1/tcpdump.txt &
-    nohup tcpdump host 172.30.37.37 > /logs/node1/tcpdump-host.txt &
-    java -Xms4092m -Xmx4092m -Druntime.nodename=node1 -Druntime.environment=${runtimeenvironment} -Djava.security.egd=file:/dev/./urandom -Duser.timezone=Europe/Helsinki -XX:-OmitStackTraceInFastThrow -jar /sillari.jar
+    java -Xms4092m -Xmx4092m -Druntime.nodename=node1 -Denv=${runtimeenvironment} -Druntime.environment=${runtimeenvironment} -Djava.security.egd=file:/dev/./urandom -Duser.timezone=Europe/Helsinki -XX:-OmitStackTraceInFastThrow -jar /sillari.jar
 else
     if [ -f "/var/sillari/node2" ]; then
         nodename="node2"
         export nodename
         nohup tcpdump port 2000 > /logs/node1/tcpdump.txt &
     nohup tcpdump host 172.30.37.37 > /logs/node1/tcpdump-host.txt &
-        java -Xms4092m -Xmx4092m -Druntime.nodename=node2 -Druntime.environment=${runtimeenvironment} -Djava.security.egd=file:/dev/./urandom -Duser.timezone=Europe/Helsinki -XX:-OmitStackTraceInFastThrow -jar /sillari.jar
+        java -Xms4092m -Xmx4092m -Druntime.nodename=node2 -Denv=${runtimeenvironment} -Druntime.environment=${runtimeenvironment} -Djava.security.egd=file:/dev/./urandom -Duser.timezone=Europe/Helsinki -XX:-OmitStackTraceInFastThrow -jar /sillari.jar
     else
         nodename="nodex"
         export nodename
-        nohup tcpdump port 2000 > /logs/node1/tcpdump.txt &
-        nohup tcpdump host 172.30.37.37 > /logs/node1/tcpdump-host.txt &
-        java -Xms4092m -Xmx4092m -Druntime.nodename=nodex -Druntime.environment=${runtimeenvironment} -Djava.security.egd=file:/dev/./urandom -Duser.timezone=Europe/Helsinki -XX:-OmitStackTraceInFastThrow -jar /sillari.jar
+        java -Xms4092m -Xmx4092m -Druntime.nodename=nodex -Denv=${runtimeenvironment} -Druntime.environment=${runtimeenvironment} -Djava.security.egd=file:/dev/./urandom -Duser.timezone=Europe/Helsinki -XX:-OmitStackTraceInFastThrow -jar /sillari.jar
     fi
 fi
