88b69ec84968356cd87edbd975bda7124cdb1095
Jari Piitulainen <jari.piitulainen@cgi.com>
2021-01-11 08:54:22 +0200

    sillari-ui container

---
+++ ui/webapp/java/fi/vaylavirasto/SillariUiApp.java
@@ -0,0 +1,11 @@
+package fi.vaylavirasto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SillariUiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SillariUiApp.class, args);
+    }
+}
