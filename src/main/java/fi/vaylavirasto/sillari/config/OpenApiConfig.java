bbc6915162891f54e2dc9db5ac64d23494987db6
Anthony Brown <ant.brown@cgi.com>
2021-02-19 10:40:16 +0200

    Changed swagger ui back to using an absolute url

--- src/main/java/fi/vaylavirasto/sillari/config/OpenApiConfig.java
+++ src/main/java/fi/vaylavirasto/sillari/config/OpenApiConfig.java
@@ -9,8 +9,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 title = "SillaRi API",
                 version = "v3",
                 description = "This app provides REST APIs for SillaRi Application"
-        )
-        /*
+        ),
         servers = {
                 @Server(
                         url="https://sillaridev.testivaylapilvi.fi/api/",
@@ -21,7 +20,6 @@ import io.swagger.v3.oas.annotations.servers.Server;
                         description="Local Server"
                 )
         }
-        */
 )
 public class OpenApiConfig {
 }
