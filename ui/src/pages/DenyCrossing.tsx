2ffae53b3c588ac216e22b4ea6d3fce8cec33d72
Kalle Mustonen <kalle.mustonen@cgi.com>
2021-04-01 11:42:19 +0300

    deny crossing page - get bridge name from state

--- ui/src/pages/DenyCrossing.tsx
+++ ui/src/pages/DenyCrossing.tsx
@@ -1,14 +1,37 @@
 import { IonCol, IonContent, IonGrid, IonPage, IonRow, IonText, IonTextarea } from "@ionic/react";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { RouteComponentProps } from "react-router";
+import { useDispatch } from "react-redux";
+import { useQuery } from "@apollo/client";
+import { useTypedSelector } from "../store/store";
 import Header from "../components/Header";
+import IBridgeDetail from "../interfaces/IBridgeDetail";
+import bridgeQuery from "../graphql/BridgeQuery";
+import { actions as crossingActions } from "../store/crossingsSlice";
 
-const DenyCrossing = (): JSX.Element => {
+interface DenyCrossingProps {
+  id: string;
+}
+
+const DenyCrossing = ({ match }: RouteComponentProps<DenyCrossingProps>): JSX.Element => {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const crossingsState = useTypedSelector((state) => state.crossingsReducer);
+  const { selectedBridgeDetail } = crossingsState;
+  const { name = "", id } = selectedBridgeDetail || {};
+  const {
+    params: { id: bridgeId },
+  } = match;
+
+  useQuery<IBridgeDetail>(bridgeQuery(Number(bridgeId)), {
+    onCompleted: (response) => dispatch({ type: crossingActions.GET_BRIDGE, payload: response }),
+    onError: (err) => console.error(err),
+  });
 
   return (
     <IonPage>
-      <Header title={t("main.header.title")} />
+      <Header title={name} />
       <IonContent>
         <div className="cardListContainer" />
         <IonGrid>
