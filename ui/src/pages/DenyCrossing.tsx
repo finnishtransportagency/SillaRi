b42dbc9ac613963a36c1f13e69b1ed7b425b272e
Kalle Mustonen <kalle.mustonen@cgi.com>
2021-04-01 11:15:27 +0300

    deny crossing some structure to the page

--- ui/src/pages/DenyCrossing.tsx
+++ ui/src/pages/DenyCrossing.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonPage, IonText, IonTextarea } from "@ionic/react";
+import { IonCol, IonContent, IonGrid, IonPage, IonRow, IonText, IonTextarea } from "@ionic/react";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import Header from "../components/Header";
@@ -9,12 +9,23 @@ const DenyCrossing = (): JSX.Element => {
   return (
     <IonPage>
       <Header title={t("main.header.title")} />
-      <IonContent fullscreen>
-        <IonText>
-          <h5>{t("denyCrossing.cantCross")}</h5>
-          <p>{t("denyCrossing.whyCantCross")}</p>
-        </IonText>
-        <IonTextarea />
+      <IonContent>
+        <div className="cardListContainer" />
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonText>
+                <h5>{t("denyCrossing.cantCross")}</h5>
+                <p>{t("denyCrossing.whyCantCross")}</p>
+              </IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonTextarea />
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
