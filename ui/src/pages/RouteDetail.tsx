689dc471b570b05dbc375c785425cc6703632e82
Anthony Brown <ant.brown@cgi.com>
2021-04-07 14:28:09 +0300

    Added route detail page, without data for now

--- ui/src/pages/RouteDetail.tsx
+++ ui/src/pages/RouteDetail.tsx
@@ -1,19 +1,18 @@
+import React from "react";
 import { RouteComponentProps } from "react-router";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
-import { IonButton, IonCol, IonContent, IonGrid, IonIcon, IonList, IonPage, IonRow, IonText } from "@ionic/react";
-import React from "react";
+import { IonCheckbox, IonCol, IonContent, IonGrid, IonIcon, IonPage, IonRow, IonText } from "@ionic/react";
+import { analyticsOutline, documentTextOutline, flagOutline } from "ionicons/icons";
 import { useQuery } from "@apollo/client";
-import { useTypedSelector } from "../store/store";
 import Header from "../components/Header";
-import ICompanyDetail from "../interfaces/ICompanyDetail";
-import { actions as crossingActions } from "../store/crossingsSlice";
-import routeQuery from "../graphql/RouteQuery";
-import IRouteDetail from "../interfaces/IRouteDetail";
 import BridgeCardList from "../components/BridgeCardList";
-import IAuthorization from "../interfaces/IAuthorization";
-import authorizationQuery from "../graphql/AuthorizationQuery";
+import { authorizationQuery } from "../graphql/AuthorizationQuery";
+import { routeQuery } from "../graphql/RouteQuery";
+import IRouteDetail from "../interfaces/IRouteDetail";
 import IAuthorizationDetail from "../interfaces/IAuthorizationDetail";
+import { actions as crossingActions } from "../store/crossingsSlice";
+import { useTypedSelector } from "../store/store";
 
 interface RouteDetailProps {
   routeId: string;
@@ -23,9 +22,12 @@ interface RouteDetailProps {
 const RouteDetail = ({ match }: RouteComponentProps<RouteDetailProps>): JSX.Element => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
+
   const crossingsState = useTypedSelector((state) => state.crossingsReducer);
-  const { selectedRouteDetail } = crossingsState;
-  const { bridges = [], name = "", id } = selectedRouteDetail || {};
+  const { selectedAuthorizationDetail, selectedRouteDetail } = crossingsState;
+  const { permissionId } = selectedAuthorizationDetail || {};
+  const { bridges = [], name = "" } = selectedRouteDetail || {};
+
   const {
     params: { routeId, authorizationId },
   } = match;
@@ -38,11 +40,138 @@ const RouteDetail = ({ match }: RouteComponentProps<RouteDetailProps>): JSX.Elem
     onCompleted: (response) => dispatch({ type: crossingActions.GET_ROUTE, payload: response }),
     onError: (err) => console.error(err),
   });
+
   return (
     <IonPage>
-      <Header title={name} />
+      <Header title={`${permissionId} - ${name}`} />
       <IonContent>
-        <div className="cardListContainer" />
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonText className="headingText">{t("route.permitInfo.title")}</IonText>
+            </IonCol>
+            <IonCol size="auto">
+              <IonIcon icon={analyticsOutline} />
+              <IonText>{` ${t("company.route")}`}</IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonText>{t("route.permitInfo.permitNumber")}</IonText>
+            </IonCol>
+            <IonCol>
+              <IonText>TODO</IonText>
+              <IonText className="ion-float-right">
+                <IonIcon icon={documentTextOutline} />
+                <IonText>{" PDF"}</IonText>
+              </IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonText>{t("route.permitInfo.routeDeparturePoint")}</IonText>
+            </IonCol>
+            <IonCol>
+              <IonText>TODO</IonText>
+              <IonText className="ion-float-right">
+                <IonIcon icon={flagOutline} />
+              </IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonText>{t("route.permitInfo.routeArrivalPoint")}</IonText>
+            </IonCol>
+            <IonCol>
+              <IonText>TODO</IonText>
+              <IonText className="ion-float-right">
+                <IonIcon icon={flagOutline} />
+              </IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonText className="headingText">{t("route.transportInfo.title")} </IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonText>{t("route.transportInfo.registryIds")}</IonText>
+            </IonCol>
+            <IonCol>
+              <IonText>TODO</IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonText>{t("route.transportInfo.dimensions")}</IonText>
+            </IonCol>
+            <IonCol>
+              <IonGrid className="ion-no-padding">
+                <IonRow>
+                  <IonCol>
+                    <IonText>{`${t("route.transportInfo.height")} TODO`}</IonText>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonText>{`${t("route.transportInfo.width")} TODO`}</IonText>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonText>{`${t("route.transportInfo.length")} TODO`}</IonText>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonText>{t("route.transportInfo.totalMass")}</IonText>
+            </IonCol>
+            <IonCol>
+              <IonText>TODO</IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonText>{t("route.transportInfo.axleWeightsDistances")}</IonText>
+            </IonCol>
+            <IonCol>
+              <IonText>TODO</IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
+        <IonGrid>
+          <IonRow>
+            <IonCol size="auto">
+              <IonCheckbox />
+            </IonCol>
+            <IonCol>
+              <IonText>{t("route.transportValid")} </IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonText className="headingText">{t("route.bridgesToSupervise")} </IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
         <BridgeCardList bridges={bridges} />
       </IonContent>
     </IonPage>
