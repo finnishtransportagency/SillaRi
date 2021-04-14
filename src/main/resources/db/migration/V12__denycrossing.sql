2f3f0a875154db17ad1e6ecf7c7c07569c714062
Kalle Mustonen <kalle.mustonen@cgi.com>
2021-04-14 13:07:25 +0300

    sql deny crossing reason field

--- src/main/resources/db/migration/V12__denycrossing.sql
+++ src/main/resources/db/migration/V12__denycrossing.sql
@@ -1,2 +1,4 @@
 alter table sillari.crossing add column IF NOT EXISTS denied boolean;
+alter table sillari.crossing add column IF NOT EXISTS deny_reason text;
 update sillari.crossing set denied=false;
+
