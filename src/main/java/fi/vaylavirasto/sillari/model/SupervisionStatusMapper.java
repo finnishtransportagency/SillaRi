245167dae206943c65cf0d6a70daaebd6953d85e
Noora Oksanen <noora.oksanen@cgi.com>
2021-05-12 12:29:30 +0300

    Add supervision status table and mappers for most recent status and whole status history (WIP)

---
+++ src/main/java/fi/vaylavirasto/sillari/model/SupervisionStatusMapper.java
@@ -0,0 +1,21 @@
+package fi.vaylavirasto.sillari.model;
+
+import fi.vaylavirasto.sillari.model.tables.SupervisionStatus;
+import org.jetbrains.annotations.Nullable;
+import org.jooq.Record;
+import org.jooq.RecordMapper;
+
+public class SupervisionStatusMapper implements RecordMapper<Record, SupervisionStatusModel> {
+    public static final SupervisionStatus supervisionStatus = Tables.SUPERVISION_STATUS.as("s");
+
+    @Nullable
+    @Override
+    public SupervisionStatusModel map(Record record) {
+        SupervisionStatusModel supervisionStatusModel = new SupervisionStatusModel();
+        supervisionStatusModel.setId(record.get(supervisionStatus.ID));
+        supervisionStatusModel.setSupervisionId(record.get(supervisionStatus.SUPERVISION_ID));
+        supervisionStatusModel.setStatus(record.get(supervisionStatus.STATUS, new SupervisionStatusTypeConverter(String.class, SupervisionStatusType.class)));
+        supervisionStatusModel.setTime(record.get(supervisionStatus.TIME));
+        return supervisionStatusModel;
+    }
+}
