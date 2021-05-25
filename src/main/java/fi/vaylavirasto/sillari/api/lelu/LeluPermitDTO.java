dfc1de11b6ca6664dccd5b0f9c6e8c2556994803
mustonenk <kalle.mustonen@cgi.com>
2021-05-25 09:51:52 +0300

    LELU

---
+++ src/main/java/fi/vaylavirasto/sillari/api/lelu/LeluPermitDTO.java
@@ -0,0 +1,22 @@
+package fi.vaylavirasto.sillari.api.lelu;
+
+import fi.vaylavirasto.sillari.model.RouteModel;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class LeluPermitDTO {
+
+}
