f9abf88a4b276b8db505e0c054ff1abcffe5330a
Kalle Mustonen <kalle.mustonen@cgi.com>
2021-05-31 16:30:49 +0300

    transport update; left joins also in getCompanysPermits

--- src/main/java/fi/vaylavirasto/sillari/repositories/PermitRepository.java
+++ src/main/java/fi/vaylavirasto/sillari/repositories/PermitRepository.java
@@ -18,7 +18,11 @@ public class PermitRepository {
     private DSLContext dsl;
 
     public List<PermitModel> getCompanysPermits(Integer companyId) {
-        return dsl.selectFrom(PermitMapper.permit)
+        return dsl.select().from(PermitMapper.permit)
+                .leftJoin(PermitMapper.axleChart)
+                .on(PermitMapper.permit.ID.eq(PermitMapper.axleChart.PERMIT_ID))
+                .leftJoin(PermitMapper.transportDimensions)
+                .on(PermitMapper.permit.ID.eq(PermitMapper.transportDimensions.PERMIT_ID))
                 .where(PermitMapper.permit.COMPANY_ID.eq(companyId))
                 .fetch(new PermitMapper());
     }
