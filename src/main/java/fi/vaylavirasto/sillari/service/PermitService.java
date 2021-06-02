2ffd6aeb6f0363170ff0f2190f50bc11d34d7c4b
mustonenk <kalle.mustonen@cgi.com>
2021-06-02 13:41:55 +0300

    vehicle repo

--- src/main/java/fi/vaylavirasto/sillari/service/PermitService.java
+++ src/main/java/fi/vaylavirasto/sillari/service/PermitService.java
@@ -2,6 +2,8 @@ package fi.vaylavirasto.sillari.service;
 
 import fi.vaylavirasto.sillari.model.AxleModel;
 import fi.vaylavirasto.sillari.model.PermitModel;
+import fi.vaylavirasto.sillari.model.VehicleMapper;
+import fi.vaylavirasto.sillari.model.VehicleModel;
 import fi.vaylavirasto.sillari.repositories.PermitRepository;
 import fi.vaylavirasto.sillari.repositories.AxleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +19,17 @@ public class PermitService {
     @Autowired
     AxleRepository axleRepository;
 
+    @Autowired
+    VehicleRepository vehicleRepository;
+
     public PermitModel getPermit(Integer permitId) {
         PermitModel permitModel = permitRepository.getPermit(permitId);
         if (permitModel != null) {
             List<AxleModel> axles = axleRepository.getAxlesOfChart(Long.valueOf(permitModel.getAxleChart().getId()).intValue());
+            permitModel.setAxles(axles);
+        }
+        if (permitModel != null) {
+            List<VehicleModel> axles = axleRepository.getAxlesOfChart(Long.valueOf(permitModel.getAxleChart().getId()).intValue());
             permitModel.setAxles(axles);
         }
         return permitModel;
