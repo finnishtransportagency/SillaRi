68ec03cb26e043b0a664d65fa6ae25f2db95e6e1
mustonenk <kalle.mustonen@cgi.com>
2021-06-04 16:44:17 +0300

    lelu api versioning errro hndlr

--- src/main/java/fi/vaylavirasto/sillari/api/rest/error/RestExceptionHandler.java
+++ src/main/java/fi/vaylavirasto/sillari/api/rest/error/RestExceptionHandler.java
@@ -1,10 +1,8 @@
 package fi.vaylavirasto.sillari.api.rest.error;
 
 import fi.vaylavirasto.sillari.api.rest.error.APIVersionException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,25 +16,22 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.*;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-    private static final Logger LOGGER = LoggerFactory.getLogger(RestExceptionHandler.class);
-
-    @Autowired
-    private MessageSource messageSource;
+    private static final Logger logger = LogManager.getLogger();
 
     @Override
     @NonNull
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   @NonNull HttpHeaders headers,
-                                                                  HttpStatus status, @NonNull WebRequest request) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", new Date());
-        body.put("status", status.value());
-
+                                                                  @NonNull HttpStatus status,
+                                                                  @NonNull WebRequest request) {
         // Get all errors
         List<String> errors = ex.getBindingResult()
                 .getFieldErrors()
@@ -44,33 +39,35 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.toList());
 
-        body.put("errors", errors);
+        logger.warn("MethodArgumentNotValidException 'errors':'{}', 'headers':'{}', 'status':'{}'", errors, headers, status);
 
-        LOGGER.warn("MethodArgumentNotValidException 'errors':'{}', 'headers':'{}', 'status':'{}'", errors, headers, status);
-        return new ResponseEntity<>(body, headers, status);
+        return createErrorResponse(status, null, errors);
     }
 
     // Let Spring handle the exception, we just override the status code
     @ExceptionHandler(Exception.class)
     public void other(Exception ex, HttpServletResponse response) throws IOException {
-        LOGGER.error("General Exception: ", ex);
+        logger.error("General Exception: ", ex);
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
 
     @ExceptionHandler(APIVersionException.class)
     public ResponseEntity<Object> apiVersionException(APIVersionException ex) {
-        LOGGER.error("apiVersionException 'reason':'{}'", ex.getMessage());
-        return handleCustomException(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        logger.error("apiVersionException 'reason':'{}'", ex.getMessage());
+        return createErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), null);
     }
 
-    private ResponseEntity<Object> handleCustomException(String message, HttpStatus status) {
+    private ResponseEntity<Object> createErrorResponse(HttpStatus status, String message, List<String> errors) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", new Date());
         body.put("status", status.value());
-        body.put("message", message);
+        if (message != null) {
+            body.put("message", message);
+        }
+        if (errors != null && !errors.isEmpty()) {
+            body.put("errors", errors);
+        }
         return new ResponseEntity<>(body, status);
     }
-
 }
-
