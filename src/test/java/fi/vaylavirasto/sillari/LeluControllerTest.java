dd3e1c47331ed51b58868c9e500cba0e3bd935a4
mustonenk <kalle.mustonen@cgi.com>
2021-06-04 11:16:03 +0300

    lelu api versioning

--- src/test/java/fi/vaylavirasto/sillari/LeluControllerTest.java
+++ src/test/java/fi/vaylavirasto/sillari/LeluControllerTest.java
@@ -13,17 +13,12 @@ import java.util.Collections;
 class LeluControllerTest {
 
     @Test
-    void leluClient() {
-        WebClient client = WebClient.builder()
-                .baseUrl("http://localhost:8080/api/lelu")
-                .defaultCookie("cookieKey", "cookieValue")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8080"))
-                .build();
+    void testWithCorrectVersion() {
+        WebClient client = buildClient();
 
         String responseString = client.get()
-                .uri("/testGet")
-                .header("accept-version", "v1")
+                .uri("/testGetWithVersion")
+                .header("accept-version", "0.1.0")
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
@@ -31,6 +26,46 @@ class LeluControllerTest {
         System.out.println("HEllo:"+ responseString);
 
 
+    }
+
+    @Test
+    void testWithOldVersion() {
+        WebClient client = buildClient();
+
+        String responseString = client.get()
+                .uri("/testGetWithVersion")
+                .header("accept-version", "0.0.0")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println("HEllo:"+ responseString);
+
+
+    }
+
+    @Test
+    void testWithNoVersion() {
+        WebClient client = buildClient();
+
+        String responseString = client.get()
+                .uri("/testGetWithVersion")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println("HEllo:"+ responseString);
+
+
+    }
+
+    private WebClient buildClient(){
+        return WebClient.builder()
+                .baseUrl("http://localhost:8080/api/lelu")
+                .defaultCookie("cookieKey", "cookieValue")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8080"))
+                .build();
     }
 
 }
