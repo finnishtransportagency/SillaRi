6b736cf92649698128aaa3b066717ff22b14e85e
mustonenk <kalle.mustonen@cgi.com>
2021-06-04 11:30:35 +0300

    lelu api versioning

---
+++ src/main/java/fi/vaylavirasto/sillari/util/SemanticVersioningUtil.java
@@ -0,0 +1,45 @@
+package fi.vaylavirasto.sillari.util;
+
+public class SemanticVersioningUtil {
+    private static final String SEPARATOR = ".";
+
+    public static String getMajorVersion(String versionNumber){
+        if(versionNumber == null){
+            return null;
+        }
+        String[] splitted = versionNumber.split(SEPARATOR);
+        if(splitted.length ==0){
+            return null;
+        }
+        else{
+            return splitted[0];
+        }
+    }
+
+
+    public static boolean matchesMajorVersion(String versionNumber1, String versionNumber2){
+
+        var majorVersion1 = getMajorVersion(versionNumber1);
+        var majorVersion2 = getMajorVersion(versionNumber2);
+
+        if(majorVersion1 == null){
+            if(majorVersion2 ==null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        if(majorVersion2 == null){
+            if(majorVersion1 ==null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        return majorVersion1.equals(majorVersion2);
+    }
+}
