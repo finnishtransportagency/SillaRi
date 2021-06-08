b0ec7850b2aac0c83e0212d12c07018bb44e6097
mustonenk <kalle.mustonen@cgi.com>
2021-06-08 10:31:45 +0300

    swagger

--- src/main/java/fi/vaylavirasto/sillari/api/rest/LeluController.java
+++ src/main/java/fi/vaylavirasto/sillari/api/rest/LeluController.java
@@ -5,6 +5,8 @@ import fi.vaylavirasto.sillari.api.rest.error.APIVersionException;
 import fi.vaylavirasto.sillari.service.LeluService;
 import fi.vaylavirasto.sillari.util.SemanticVersioningUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,6 +83,9 @@ public class LeluController {
             "If permit is updated, updates routes found with same LeLu ID, adds new routes and deletes routes that are no longer included in the permit. " +
             "CURRENT LIMITATIONS: 1. Bridge OID must be found in SillaRi DB, otherwise bridge is not added. " +
             "2. Updated routes must not have existing transport instances or supervisions.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "HttpStatus.BAD_REQUEST", description = "API version mismatch"),
+    })
     public void savePermit(@Valid @RequestBody LeluPermitDTO permitDTO, @RequestHeader(value = LELU_API_VERSION_HEADER_NAME, required = false) String apiVersion) throws APIVersionException {
         if (apiVersion == null || SemanticVersioningUtil.matchesMajorVersion(apiVersion, currentApiVersion)) {
             logger.debug("LeLu savePermit='number':'{}', 'version':{}", permitDTO.getNumber(), permitDTO.getVersion());
