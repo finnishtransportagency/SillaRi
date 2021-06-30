5b1607ca4558c025a5fd19d14b0cb2834565dd49
Anthony Brown <ant.brown@cgi.com>
2021-06-30 15:52:46 +0300

    Changed backend api methods to return empty json instead of null

--- src/main/java/fi/vaylavirasto/sillari/api/rest/BridgeController.java
+++ src/main/java/fi/vaylavirasto/sillari/api/rest/BridgeController.java
@@ -2,10 +2,13 @@ package fi.vaylavirasto.sillari.api.rest;
 
 import fi.vaylavirasto.sillari.api.ServiceMetric;
 import fi.vaylavirasto.sillari.model.BridgeModel;
+import fi.vaylavirasto.sillari.model.EmptyJsonResponse;
 import fi.vaylavirasto.sillari.service.BridgeService;
 import io.micrometer.core.annotation.Timed;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +23,13 @@ public class BridgeController {
     BridgeService bridgeService;
 
     @Operation(summary = "Get bridge")
-    @GetMapping("/getbridge")
+    @GetMapping(value = "/getbridge", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("@sillariRightsChecker.isSillariUser(authentication)")
-    public BridgeModel getBridge(@RequestParam Integer bridgeId) {
+    public ResponseEntity<?> getBridge(@RequestParam Integer bridgeId) {
         ServiceMetric serviceMetric = new ServiceMetric("BridgeController", "getBridge");
         try {
-            return bridgeService.getBridge(bridgeId);
+            BridgeModel bridge = bridgeService.getBridge(bridgeId);
+            return ResponseEntity.ok().body(bridge != null ? bridge : new EmptyJsonResponse());
         } finally {
             serviceMetric.end();
         }
--- src/main/java/fi/vaylavirasto/sillari/api/rest/RouteBridgeController.java
+++ src/main/java/fi/vaylavirasto/sillari/api/rest/RouteBridgeController.java
@@ -1,11 +1,14 @@
 package fi.vaylavirasto.sillari.api.rest;
 
 import fi.vaylavirasto.sillari.api.ServiceMetric;
+import fi.vaylavirasto.sillari.model.EmptyJsonResponse;
 import fi.vaylavirasto.sillari.model.RouteBridgeModel;
 import fi.vaylavirasto.sillari.service.RouteBridgeService;
 import io.micrometer.core.annotation.Timed;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +23,13 @@ public class RouteBridgeController {
     RouteBridgeService routeBridgeService;
 
     @Operation(summary = "Get route bridge")
-    @GetMapping("/getroutebridge")
+    @GetMapping(value = "/getroutebridge", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("@sillariRightsChecker.isSillariUser(authentication)")
-    public RouteBridgeModel getRouteBridge(@RequestParam Integer routeBridgeId) {
+    public ResponseEntity<?> getRouteBridge(@RequestParam Integer routeBridgeId) {
         ServiceMetric serviceMetric = new ServiceMetric("RouteBridgeController", "getRouteBridge");
         try {
-            return routeBridgeService.getRouteBridge(routeBridgeId);
+            RouteBridgeModel routeBridge = routeBridgeService.getRouteBridge(routeBridgeId);
+            return ResponseEntity.ok().body(routeBridge != null ? routeBridge : new EmptyJsonResponse());
         } finally {
             serviceMetric.end();
         }
