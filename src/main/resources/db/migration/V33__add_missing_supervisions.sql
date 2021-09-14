0f96204c0ad4933f646e54d7cf88d27acda664af
Noora Oksanen <noora.oksanen@cgi.com>
2021-09-14 12:36:00 +0300

    Drop table crossing

--- src/main/resources/db/migration/V33__add_missing_supervisions.sql
+++ src/main/resources/db/migration/V33__add_missing_supervisions.sql
@@ -1,5 +1,5 @@
 insert into sillari.supervision (route_bridge_id, route_transport_id, planned_time, conforms_to_permit)
-select rb.id, rt.id, '2021-09-22 06:06:06 +03:00', true -- TODO change to false
+select rb.id, rt.id, '2021-09-22 06:06:06 +03:00', false
 from sillari.route_bridge rb left join sillari.supervision s on rb.id = s.route_bridge_id
                              inner join sillari.route r on rb.route_id = r.id
                              inner join sillari.route_transport rt on r.id = rt.route_id
