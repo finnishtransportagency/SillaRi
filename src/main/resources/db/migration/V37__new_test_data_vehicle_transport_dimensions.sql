518697441a5c31844ec36755a8c5ec61768a0e7a
Noora Oksanen <noora.oksanen@cgi.com>
2021-09-20 16:10:10 +0300

    Simplify new test data inserts

--- src/main/resources/db/migration/V37__new_test_data_vehicle_transport_dimensions.sql
+++ src/main/resources/db/migration/V37__new_test_data_vehicle_transport_dimensions.sql
@@ -1,86 +1,28 @@
 --VEHICLE
-INSERT INTO sillari.vehicle
-    (permit_id, "type", identifier)
-SELECT p.id, 'TRUCK', 'ABC-963'
-FROM sillari.permit p
-WHERE permit_number = 'MV/176/2021';
+-- Reset sequence
+SELECT setval('sillari.vehicle_id_seq', 1, false);
 
-INSERT INTO sillari.vehicle
-    (permit_id, "type", identifier)
-SELECT p.id, 'TRUCK', 'DEF-852'
-FROM sillari.permit p
-WHERE permit_number = 'MV/177/2021';
-
-INSERT INTO sillari.vehicle
-    (permit_id, "type", identifier)
-SELECT p.id, 'TRUCK', 'GHI-741'
-FROM sillari.permit p
-WHERE permit_number = '149/2021';
-
-INSERT INTO sillari.vehicle
-    (permit_id, "type", identifier)
-SELECT p.id, 'SEMITRAILER', 'JKL-630'
-FROM sillari.permit p
-WHERE permit_number = '149/2021';
-
-INSERT INTO sillari.vehicle
-    (permit_id, "type", identifier)
-SELECT p.id, 'TRUCK', 'MNO-529'
-FROM sillari.permit p
-WHERE permit_number = '150/2021';
-
-INSERT INTO sillari.vehicle
-    (permit_id, "type", identifier)
-SELECT p.id, 'TRAILER', 'PQR-418'
-FROM sillari.permit p
-WHERE permit_number = '150/2021';
+INSERT INTO sillari.vehicle (permit_id, "type", identifier) VALUES (1, 'TRUCK', 'ABC-963');
+INSERT INTO sillari.vehicle (permit_id, "type", identifier) VALUES (2, 'TRUCK', 'DEF-852');
+INSERT INTO sillari.vehicle (permit_id, "type", identifier) VALUES (3, 'TRUCK', 'GHI-741');
+INSERT INTO sillari.vehicle (permit_id, "type", identifier) VALUES (3, 'SEMITRAILER', 'JKL-630');
+INSERT INTO sillari.vehicle (permit_id, "type", identifier) VALUES (4, 'TRUCK', 'MNO-529');
+INSERT INTO sillari.vehicle (permit_id, "type", identifier) VALUES (4, 'TRAILER', 'PQR-418');
 
 --TRANSPORT_DIMENSIONS
-INSERT INTO sillari.transport_dimensions
-    (permit_id, height, width, length)
-SELECT p.id, 4.4, 2.5, 7.5
-FROM sillari.permit p
-WHERE permit_number = 'MV/176/2021';
+-- Reset sequence
+SELECT setval('sillari.transport_dimensions_id_seq', 1, false);
 
-INSERT INTO sillari.transport_dimensions
-    (permit_id, height, width, length)
-SELECT p.id, 4.2, 2.7, 16.5
-FROM sillari.permit p
-WHERE permit_number = 'MV/177/2021';
-
-INSERT INTO sillari.transport_dimensions
-    (permit_id, height, width, length)
-SELECT p.id, 2.9, 3.0, 13.5
-FROM sillari.permit p
-WHERE permit_number = '149/2021';
-
-INSERT INTO sillari.transport_dimensions
-    (permit_id, height, width, length)
-SELECT p.id, 4.9, 4.5, 30.5
-FROM sillari.permit p
-WHERE permit_number = '150/2021';
+INSERT INTO sillari.transport_dimensions (permit_id, height, width, length) VALUES (1, 4.4, 2.5, 7.5);
+INSERT INTO sillari.transport_dimensions (permit_id, height, width, length) VALUES (2, 4.2, 2.7, 16.5);
+INSERT INTO sillari.transport_dimensions (permit_id, height, width, length) VALUES (3, 2.9, 3.0, 13.5);
+INSERT INTO sillari.transport_dimensions (permit_id, height, width, length) VALUES (4, 4.9, 4.5, 30.5);
 
 --UNLOADED_TRANSPORT_DIMENSIONS
-INSERT INTO sillari.unloaded_transport_dimensions
-    (permit_id, height, width, length)
-SELECT p.id, 4.2, 2.5, 7.5
-FROM sillari.permit p
-WHERE permit_number = 'MV/176/2021';;
+-- Reset sequence
+SELECT setval('sillari.unloaded_transport_dimensions_id_seq', 1, false);
 
-INSERT INTO sillari.unloaded_transport_dimensions
-    (permit_id, height, width, length)
-SELECT p.id, 4.2, 2.5, 16
-FROM sillari.permit p
-WHERE permit_number = 'MV/177/2021';
-
-INSERT INTO sillari.unloaded_transport_dimensions
-    (permit_id, height, width, length)
-SELECT p.id, 2.9, 2.6, 12.5
-FROM sillari.permit p
-WHERE permit_number = '149/2021';
-
-INSERT INTO sillari.unloaded_transport_dimensions
-    (permit_id, height, width, length)
-SELECT p.id, 4.2, 2.6, 30.5
-FROM sillari.permit p
-WHERE permit_number = '150/2021';
+INSERT INTO sillari.unloaded_transport_dimensions (permit_id, height, width, length) VALUES (1, 4.2, 2.5, 7.5);
+INSERT INTO sillari.unloaded_transport_dimensions (permit_id, height, width, length) VALUES (2, 4.2, 2.5, 16);
+INSERT INTO sillari.unloaded_transport_dimensions (permit_id, height, width, length) VALUES (3, 2.9, 2.6, 12.5);
+INSERT INTO sillari.unloaded_transport_dimensions (permit_id, height, width, length) VALUES (4, 4.2, 2.6, 30.5);
