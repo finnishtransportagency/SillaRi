5d24895daccbeb49283342468f99146a5ea1e601
Anthony Brown <ant.brown@cgi.com>
2021-09-20 15:05:57 +0300

    Added logout button to test service worker unregistration

--- ui/src/components/SidebarMenu.tsx
+++ ui/src/components/SidebarMenu.tsx
@@ -1,11 +1,29 @@
 import { IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonTitle, IonToolbar } from "@ionic/react";
 import React from "react";
-import "./SidebarMenu.css";
-import { calendar, home, settings } from "ionicons/icons";
+import { useHistory } from "react-router-dom";
+import { calendar, home, logOutOutline, settings } from "ionicons/icons";
 import { useTranslation } from "react-i18next";
+import Cookies from "js-cookie";
+import { unregister } from "../serviceWorkerRegistration";
+import "./SidebarMenu.css";
 
 const SidebarMenu: React.FC = () => {
   const { t } = useTranslation();
+  const history = useHistory();
+
+  const logout = () => {
+    // Unregister service worker
+    unregister(() => {
+      // Remove all cookies for this site
+      const cookies = Cookies.get();
+      Object.keys(cookies).forEach((key) => {
+        Cookies.remove(key);
+      });
+
+      // Reload the page
+      history.go(0);
+    });
+  };
 
   return (
     <IonMenu disabled={false} hidden={false} side="start" content-id="MainContent">
@@ -32,6 +50,12 @@ const SidebarMenu: React.FC = () => {
             <IonItem routerLink="/management/1">
               <IonIcon icon={calendar} slot="start" />
               <IonLabel>{t("SidebarMenu.management")}</IonLabel>
+            </IonItem>
+          </IonMenuToggle>
+          <IonMenuToggle>
+            <IonItem button onClick={logout}>
+              <IonIcon icon={logOutOutline} slot="start" />
+              <IonLabel>{t("SidebarMenu.logout")}</IonLabel>
             </IonItem>
           </IonMenuToggle>
         </IonList>
