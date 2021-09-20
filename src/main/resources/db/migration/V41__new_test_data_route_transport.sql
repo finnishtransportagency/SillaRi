7c1afa9f3396f43b3c890e14f58817b013279b5d
mustonenk <Lncj6324Z>
2021-09-20 15:05:28 +0300

    new test data

---
+++ src/main/resources/db/migration/V41__new_test_data_route_transport.sql
@@ -0,0 +1,5 @@
+INSERT INTO sillari.route_transport (id, route_id, planned_departure_time) OVERRIDING SYSTEM VALUE VALUES (1, 1, null);
+INSERT INTO sillari.route_transport (id, route_id, planned_departure_time) OVERRIDING SYSTEM VALUE VALUES (2, 2, null);
+INSERT INTO sillari.route_transport (id, route_id, planned_departure_time) OVERRIDING SYSTEM VALUE VALUES (3, 3, null);
+INSERT INTO sillari.route_transport (id, route_id, planned_departure_time) OVERRIDING SYSTEM VALUE VALUES (4, 4, null);
+INSERT INTO sillari.route_transport (id, route_id, planned_departure_time) OVERRIDING SYSTEM VALUE VALUES (5, 5, null);
