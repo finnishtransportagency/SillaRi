05877c85f3a856dc0320756f85231378621ea43e
Noora Oksanen <noora.oksanen@cgi.com>
2021-09-21 11:17:33 +0300

    Update test data postal codes

--- src/main/resources/db/migration/V41__new_test_data_address.sql
+++ src/main/resources/db/migration/V41__new_test_data_address.sql
@@ -1,8 +1,8 @@
-insert into sillari.address(street,postalcode,city) values(	'Hämeenpuisto 1'	,	'321'	,	'TAMPERE'	);
-insert into sillari.address(street,postalcode,city) values(	'Kallontie 14'	,	'921'	,	'PORI'	);
-insert into sillari.address(street,postalcode,city) values(	'Lentäjänpolku 2'	,	'543'	,	'KUOPIO'	);
-insert into sillari.address(street,postalcode,city) values(	'Hangontie 1'	,	'888'	,	'UUSIKAUPUNKI'	);
-insert into sillari.address(street,postalcode,city) values(	'Vanhankirkontie 2 1'	,	'999'	,	'KEURUU'	);
-insert into sillari.address(street,postalcode,city) values(	'Ihalante 1'	,	'772'	,	'MULTIA'	);
-insert into sillari.address(street,postalcode,city) values(	'Hämeenpuisto 1'	,	'321'	,	'TAMPERE'	);
-insert into sillari.address(street,postalcode,city) values(	'Kallontie 14'	,	'921'	,	'PORI'	);
+insert into sillari.address(street,postalcode,city) values(	'Hämeenpuisto 1'	,	'33210'	,	'TAMPERE'	);
+insert into sillari.address(street,postalcode,city) values(	'Kallontie 14'	,	'28880'	,	'PORI'	);
+insert into sillari.address(street,postalcode,city) values(	'Lentäjänpolku 2'	,	'70910'	,	'KUOPIO'	);
+insert into sillari.address(street,postalcode,city) values(	'Hangontie 1'	,	'23500'	,	'UUSIKAUPUNKI'	);
+insert into sillari.address(street,postalcode,city) values(	'Vanhankirkontie 2'	,	'42700'	,	'KEURUU'	);
+insert into sillari.address(street,postalcode,city) values(	'Ihalantie 1'	,	'42600'	,	'MULTIA'	);
+insert into sillari.address(street,postalcode,city) values(	'Hämeenpuisto 1'	,	'33210'	,	'TAMPERE'	);
+insert into sillari.address(street,postalcode,city) values(	'Kallontie 14'	,	'28880'	,	'PORI'	);
