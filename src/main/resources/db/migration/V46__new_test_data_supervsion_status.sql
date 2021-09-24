e6ef3154077e4d9b2c8f5218312c5f9365f42b14
mustonenk <kalle.mustonen@cgi.com>
2021-09-24 08:37:08 +0300

    supervision status test data sql insert

---
+++ src/main/resources/db/migration/V46__new_test_data_supervsion_status.sql
@@ -0,0 +1,37 @@
+insert into sillari.supervision_status (supervision_id, status, time)
+values (1, 'IN_PROGRESS', '2021-09-23 06:06:06 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (1, 'IN_PROGRESS', '2021-09-23 06:09:06 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (1, 'IN_PROGRESS', '2021-09-23 07:06:06 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (1, 'CANCELLED', '2021-09-24 07:06:06 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (1, 'IN_PROGRESS', '2021-09-24 08:06:06 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (1, 'CANCELLED', '2021-09-24 19:06:06 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (1, 'IN_PROGRESS', '2021-09-24 19:08:06 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (1, 'IN_PROGRESS', '2021-09-24 19:10:00 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (1, 'FINISHED', '2021-09-24 19:12:00 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (1, 'FINISHED', '2021-09-24 19:12:05 +03:00');
+
+insert into sillari.supervision_status (supervision_id, status, time)
+values (2, 'IN_PROGRESS', '2021-09-23 06:06:06 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (2, 'CANCELLED', '2021-09-24 07:06:06 +03:00');
+
+insert into sillari.supervision_status (supervision_id, status, time)
+values (3, 'PLANNED', '2021-09-28 06::06 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (3, 'IN_PROGRESS', '2021-09-28 10:06:06 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (3, 'IN_PROGRESS', '2021-09-28 10:11:06 +03:00');
+
+insert into sillari.supervision_status (supervision_id, status, time)
+values (4, 'IN_PROGRESS', '2021-09-30 10:06:06 +03:00');
+insert into sillari.supervision_status (supervision_id, status, time)
+values (4, 'FINISHED', '2021-09-30 11:11:06 +03:00');
