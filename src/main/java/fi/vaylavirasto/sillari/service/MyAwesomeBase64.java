8f04c0f3a9703acf547870411bb9cb5d6e8625de
mustonenk <Lncj6324Z>
2021-09-29 16:54:56 +0300

    Handle base64 encoded route zips. Custom decoder for debug.

--- src/main/java/fi/vaylavirasto/sillari/service/MyAwesomeBase64.java
+++ src/main/java/fi/vaylavirasto/sillari/service/MyAwesomeBase64.java
@@ -3,14 +3,10 @@ package fi.vaylavirasto.sillari.service;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.FilterOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.Objects;
+
 
 public class MyAwesomeBase64 {
     private MyAwesomeBase64() {
@@ -18,38 +14,7 @@ public class MyAwesomeBase64 {
 
     private static final Logger logger = LogManager.getLogger();
 
-    public static Encoder getEncoder() {
-        return Encoder.RFC4648;
-    }
 
-    public static Encoder getUrlEncoder() {
-        return Encoder.RFC4648_URLSAFE;
-    }
-
-    public static Encoder getMimeEncoder() {
-        return Encoder.RFC2045;
-    }
-
-    public static Encoder getMimeEncoder(int lineLength, byte[] lineSeparator) {
-        Objects.requireNonNull(lineSeparator);
-        int[] base64 = Decoder.fromBase64;
-        byte[] var3 = lineSeparator;
-        int var4 = lineSeparator.length;
-
-        for (int var5 = 0; var5 < var4; ++var5) {
-            byte b = var3[var5];
-            if (base64[b & 255] != -1) {
-                throw new IllegalArgumentException("Illegal base64 line separator character 0x" + Integer.toString(b, 16));
-            }
-        }
-
-        lineLength &= -4;
-        if (lineLength <= 0) {
-            return Encoder.RFC4648;
-        } else {
-            return new Encoder(false, lineSeparator, lineLength, true);
-        }
-    }
 
     public static Decoder getDecoder() {
         return Decoder.RFC4648;
@@ -57,277 +22,7 @@ public class MyAwesomeBase64 {
 
 
 
-    private static class DecInputStream extends InputStream {
-        private final InputStream is;
-        private final boolean isMIME;
-        private final int[] base64;
-        private int bits = 0;
-        private int nextin = 18;
-        private int nextout = -8;
-        private boolean eof = false;
-        private boolean closed = false;
-        private byte[] sbBuf = new byte[1];
 
-        DecInputStream(InputStream is, int[] base64, boolean isMIME) {
-            this.is = is;
-            this.base64 = base64;
-            this.isMIME = isMIME;
-        }
-
-        public int read() throws IOException {
-            return this.read(this.sbBuf, 0, 1) == -1 ? -1 : this.sbBuf[0] & 255;
-        }
-
-        private int eof(byte[] b, int off, int len, int oldOff) throws IOException {
-            this.eof = true;
-            if (this.nextin != 18) {
-                if (this.nextin == 12) {
-                    throw new IOException("Base64 stream has one un-decoded dangling byte.");
-                }
-
-                b[off++] = (byte) (this.bits >> 16);
-                if (this.nextin == 0) {
-                    if (len == 1) {
-                        this.bits >>= 8;
-                        this.nextout = 0;
-                    } else {
-                        b[off++] = (byte) (this.bits >> 8);
-                    }
-                }
-            }
-
-            return off == oldOff ? -1 : off - oldOff;
-        }
-
-        private int padding(byte[] b, int off, int len, int oldOff) throws IOException {
-            if (this.nextin == 18 || this.nextin == 12 || this.nextin == 6 && this.is.read() != 61) {
-                throw new IOException("Illegal base64 ending sequence:" + this.nextin);
-            } else {
-                b[off++] = (byte) (this.bits >> 16);
-                if (this.nextin == 0) {
-                    if (len == 1) {
-                        this.bits >>= 8;
-                        this.nextout = 0;
-                    } else {
-                        b[off++] = (byte) (this.bits >> 8);
-                    }
-                }
-
-                this.eof = true;
-                return off - oldOff;
-            }
-        }
-
-        public int read(byte[] b, int off, int len) throws IOException {
-            if (this.closed) {
-                throw new IOException("Stream is closed");
-            } else if (this.eof && this.nextout < 0) {
-                return -1;
-            } else if (off >= 0 && len >= 0 && len <= b.length - off) {
-                int oldOff;
-                for (oldOff = off; this.nextout >= 0; this.nextout -= 8) {
-                    if (len == 0) {
-                        return off - oldOff;
-                    }
-
-                    b[off++] = (byte) (this.bits >> this.nextout);
-                    --len;
-                }
-
-                this.bits = 0;
-
-                while (len > 0) {
-                    int v = this.is.read();
-                    if (v == -1) {
-                        return this.eof(b, off, len, oldOff);
-                    }
-
-                    if ((v = this.base64[v]) < 0) {
-                        if (v == -2) {
-                            return this.padding(b, off, len, oldOff);
-                        }
-
-                        if (v == -1) {
-                            if (!this.isMIME) {
-                                throw new IOException("Illegal base64 character " + Integer.toString(v, 16));
-                            }
-                            continue;
-                        }
-                    }
-
-                    this.bits |= v << this.nextin;
-                    if (this.nextin == 0) {
-                        this.nextin = 18;
-                        b[off++] = (byte) (this.bits >> 16);
-                        if (len == 1) {
-                            this.nextout = 8;
-                            break;
-                        }
-
-                        b[off++] = (byte) (this.bits >> 8);
-                        if (len == 2) {
-                            this.nextout = 0;
-                            break;
-                        }
-
-                        b[off++] = (byte) this.bits;
-                        len -= 3;
-                        this.bits = 0;
-                    } else {
-                        this.nextin -= 6;
-                    }
-                }
-
-                return off - oldOff;
-            } else {
-                throw new IndexOutOfBoundsException();
-            }
-        }
-
-        public int available() throws IOException {
-            if (this.closed) {
-                throw new IOException("Stream is closed");
-            } else {
-                return this.is.available();
-            }
-        }
-
-        public void close() throws IOException {
-            if (!this.closed) {
-                this.closed = true;
-                this.is.close();
-            }
-
-        }
-    }
-
-    private static class EncOutputStream extends FilterOutputStream {
-        private int leftover = 0;
-        private int b0;
-        private int b1;
-        private int b2;
-        private boolean closed = false;
-        private final char[] base64;
-        private final byte[] newline;
-        private final int linemax;
-        private final boolean doPadding;
-        private int linepos = 0;
-        private byte[] buf;
-
-        EncOutputStream(OutputStream os, char[] base64, byte[] newline, int linemax, boolean doPadding) {
-            super(os);
-            this.base64 = base64;
-            this.newline = newline;
-            this.linemax = linemax;
-            this.doPadding = doPadding;
-            this.buf = new byte[linemax <= 0 ? 8124 : linemax];
-        }
-
-        public void write(int b) throws IOException {
-            byte[] buf = new byte[]{(byte) (b & 255)};
-            this.write(buf, 0, 1);
-        }
-
-        private void checkNewline() throws IOException {
-            if (this.linepos == this.linemax) {
-                this.out.write(this.newline);
-                this.linepos = 0;
-            }
-
-        }
-
-        private void writeb4(char b1, char b2, char b3, char b4) throws IOException {
-            this.buf[0] = (byte) b1;
-            this.buf[1] = (byte) b2;
-            this.buf[2] = (byte) b3;
-            this.buf[3] = (byte) b4;
-            this.out.write(this.buf, 0, 4);
-        }
-
-        public void write(byte[] b, int off, int len) throws IOException {
-            if (this.closed) {
-                throw new IOException("Stream is closed");
-            } else if (off >= 0 && len >= 0 && len <= b.length - off) {
-                if (len != 0) {
-                    if (this.leftover != 0) {
-                        if (this.leftover == 1) {
-                            this.b1 = b[off++] & 255;
-                            --len;
-                            if (len == 0) {
-                                ++this.leftover;
-                                return;
-                            }
-                        }
-
-                        this.b2 = b[off++] & 255;
-                        --len;
-                        this.checkNewline();
-                        this.writeb4(this.base64[this.b0 >> 2], this.base64[this.b0 << 4 & 63 | this.b1 >> 4], this.base64[this.b1 << 2 & 63 | this.b2 >> 6], this.base64[this.b2 & 63]);
-                        this.linepos += 4;
-                    }
-
-                    int nBits24 = len / 3;
-
-                    int dp;
-                    for (this.leftover = len - nBits24 * 3; nBits24 > 0; nBits24 -= dp / 4) {
-                        this.checkNewline();
-                        int dl = this.linemax <= 0 ? this.buf.length : this.buf.length - this.linepos;
-                        int sl = off + Math.min(nBits24, dl / 4) * 3;
-                        dp = 0;
-
-                        int bits;
-                        for (int sp = off; sp < sl; this.buf[dp++] = (byte) this.base64[bits & 63]) {
-                            bits = (b[sp++] & 255) << 16 | (b[sp++] & 255) << 8 | b[sp++] & 255;
-                            this.buf[dp++] = (byte) this.base64[bits >>> 18 & 63];
-                            this.buf[dp++] = (byte) this.base64[bits >>> 12 & 63];
-                            this.buf[dp++] = (byte) this.base64[bits >>> 6 & 63];
-                        }
-
-                        this.out.write(this.buf, 0, dp);
-                        off = sl;
-                        this.linepos += dp;
-                    }
-
-                    if (this.leftover == 1) {
-                        this.b0 = b[off++] & 255;
-                    } else if (this.leftover == 2) {
-                        this.b0 = b[off++] & 255;
-                        this.b1 = b[off++] & 255;
-                    }
-
-                }
-            } else {
-                throw new ArrayIndexOutOfBoundsException();
-            }
-        }
-
-        public void close() throws IOException {
-            if (!this.closed) {
-                this.closed = true;
-                if (this.leftover == 1) {
-                    this.checkNewline();
-                    this.out.write(this.base64[this.b0 >> 2]);
-                    this.out.write(this.base64[this.b0 << 4 & 63]);
-                    if (this.doPadding) {
-                        this.out.write(61);
-                        this.out.write(61);
-                    }
-                } else if (this.leftover == 2) {
-                    this.checkNewline();
-                    this.out.write(this.base64[this.b0 >> 2]);
-                    this.out.write(this.base64[this.b0 << 4 & 63 | this.b1 >> 4]);
-                    this.out.write(this.base64[this.b1 << 2 & 63]);
-                    if (this.doPadding) {
-                        this.out.write(61);
-                    }
-                }
-
-                this.leftover = 0;
-                this.out.close();
-            }
-
-        }
-    }
 
     public static class Decoder {
         private final boolean isURL;
@@ -557,10 +252,7 @@ public class MyAwesomeBase64 {
             }
         }
 
-        public String encodeToString(byte[] src) {
-            byte[] encoded = this.encode(src);
-            return new String(encoded, 0, 0, encoded.length);
-        }
+
 
         public ByteBuffer encode(ByteBuffer buffer) {
             int len = this.outLength(buffer.remaining());
@@ -582,10 +274,7 @@ public class MyAwesomeBase64 {
             return ByteBuffer.wrap(dst);
         }
 
-        public OutputStream wrap(OutputStream os) {
-            Objects.requireNonNull(os);
-            return new EncOutputStream(os, this.isURL ? toBase64URL : toBase64, this.newline, this.linemax, this.doPadding);
-        }
+
 
         public Encoder withoutPadding() {
             return !this.doPadding ? this : new Encoder(this.isURL, this.newline, this.linemax, false);
