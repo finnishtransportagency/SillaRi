393bac972e3daf9b18fc1ed77cd4e194c8e69720
Noora Oksanen <noora.oksanen@cgi.com>
2021-09-30 10:55:18 +0300

    Delete conflicting migration

--- src/main/resources/db/migration/V47__insert_supervisor.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO sillari.supervisor (firstname, lastname) VALUES ('Testi', 'Valvoja');
-
---Add the test supervisor to first random 10 supervisions
-INSERT INTO sillari.supervision_supervisor (supervision_id, supervisor_id, priority)
-SELECT s.id, sr.id, 1
-FROM supervision s CROSS JOIN (SELECT id FROM sillari.supervisor WHERE firstname = 'Testi' and lastname = 'Valvoja') sr
-LIMIT 10;
