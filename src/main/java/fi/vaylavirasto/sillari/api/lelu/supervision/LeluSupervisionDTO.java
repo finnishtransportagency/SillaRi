bc589898f2d8504275b66791a67d8f1bab696d70
mustonenk <kalle.mustonen@cgi.com>
2021-10-19 13:30:21 +0300

    typo

--- src/main/java/fi/vaylavirasto/sillari/api/lelu/supervision/LeluSupervisionDTO.java
+++ src/main/java/fi/vaylavirasto/sillari/api/lelu/supervision/LeluSupervisionDTO.java
@@ -6,7 +6,7 @@ public class LeluSupervisionDTO {
     @Schema(description = "Bridge crossing supervisor ")
     private LeluSupervisor supervisor;
 
-    @Schema(description = "Status of the supervision, those with status 'SIGNED' have report available from /getSupervisionReport ", example = "SINGNED")
+    @Schema(description = "Status of the supervision, those with status 'SIGNED' have report available from /getSupervisionReport ", example = "SIGNED")
     LeluSupervisionStatus status;
 
     @Schema(description = "Identifier of a report available from /getSupervisionReport ", example = "123456_abc")
