529a6994df7311d67dfd7f0b5574e6b7335b108f
mustonenk <kalle.mustonen@cgi.com>
2021-10-20 16:26:05 +0300

    insert a bridge

---
+++ src/main/resources/db/migration/V55_more_bridges.sql
@@ -0,0 +1,3 @@
+INSERT INTO sillari.bridge ("oid", identifier, "name", municipality, status, geom)
+VALUES ('1.2.246.578.1.15.205499', 'T-5499', 'Träskin putkisilta', 'Parainen', 'kaytossa',
+        'SRID=3067;POINT (187041 6688002)'::geometry);
