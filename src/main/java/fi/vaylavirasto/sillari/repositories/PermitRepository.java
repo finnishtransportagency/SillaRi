649f49fc6f76936e754c4fb441be97e2b3cbe192
mustonenk <Lncj6324Z>
2021-10-21 17:32:38 +0300

    delete transport status on transport del

--- src/main/java/fi/vaylavirasto/sillari/repositories/PermitRepository.java
+++ src/main/java/fi/vaylavirasto/sillari/repositories/PermitRepository.java
@@ -492,6 +492,13 @@ public class PermitRepository {
     }
 
     private void deleteRouteTransports(DSLContext ctx, RouteModel routeModel) {
+        ctx.delete(TableAlias.transportStatus)
+                .where(TableAlias.transportStatus.ROUTE_TRANSPORT_ID.in(ctx.select(TableAlias.routeTransport.ID)
+                .from(TableAlias.routeTransport)
+                .where(TableAlias.routeTransport.ROUTE_ID.eq(routeModel.getId()))
+                .fetch()))
+                .execute();
+
         ctx.delete(TableAlias.routeTransport)
                 .where(TableAlias.routeTransport.ROUTE_ID.eq(routeModel.getId()))
                 .execute();
