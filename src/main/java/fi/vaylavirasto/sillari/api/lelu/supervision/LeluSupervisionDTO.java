4149cb5a8407b0d4b84933a5ef3273823fc3c5db
mustonenk <Lncj6324Z>
2021-10-28 13:34:39 +0300

    remove errroneous example swagger example val

--- src/main/java/fi/vaylavirasto/sillari/api/lelu/supervision/LeluSupervisionDTO.java
+++ src/main/java/fi/vaylavirasto/sillari/api/lelu/supervision/LeluSupervisionDTO.java
@@ -12,7 +12,7 @@ public class LeluSupervisionDTO {
     @Schema(description = "Bridge crossing supervisor ")
     private LeluSupervisor supervisor;
 
-    @Schema(description = "Status of the supervision, those with status 'SIGNED' have report available from /getSupervisionReport ", example = "SIGNED")
+    @Schema(description = "Status of the supervision, those with status 'SIGNED' have report available from /getSupervisionReport ")
     LeluSupervisionStatus supervisionStatus;
 
     @Schema(description = "Identifier of a report available from /getSupervisionReport ", example = "123456")
