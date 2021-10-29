ca66dd476e9eeb3e7165328bebb41a9a0d356594
mustonenk <kalle.mustonen@cgi.com>
2021-10-29 08:35:46 +0300

    lelu supervision report changes from Aitio feedback

--- src/main/java/fi/vaylavirasto/sillari/api/lelu/supervision/LeluSupervisionStatus.java
+++ src/main/java/fi/vaylavirasto/sillari/api/lelu/supervision/LeluSupervisionStatus.java
@@ -13,7 +13,7 @@ public class LeluSupervisionStatus {
     @Schema(description = "Status timestamp", required = true, example = "2021-05-26T08:02:36.000Z")
     private OffsetDateTime modifiedDate;
 
-    @Schema(description = "Descipton of the reason for status change")
+    @Schema(description = "Description of the reason for status change")
     private String reasonText;
 }
 
