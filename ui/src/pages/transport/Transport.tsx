3badee20eb496cf518d99ea4e3dc2095e53ddc69
Anthony Brown <ant.brown@cgi.com>
2021-11-15 10:53:52 +0200

    Changed transport styling to match UI design

--- ui/src/pages/transport/Transport.tsx
+++ ui/src/pages/transport/Transport.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { useQuery } from "react-query";
 import { useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { IonButton, IonCol, IonContent, IonGrid, IonIcon, IonPage, IonRow, IonText } from "@ionic/react";
+import moment from "moment";
 import Header from "../../components/Header";
 import NoNetworkNoData from "../../components/NoNetworkNoData";
+import RouteAccordion from "../../components/RouteAccordion";
+import IRoute from "../../interfaces/IRoute";
 import { useTypedSelector } from "../../store/store";
+import arrowLeft from "../../theme/icons/arrow-left.svg";
 import { getPermitOfRouteTransport, getRouteTransport, onRetry } from "../../utils/managementBackendData";
-import moment from "moment";
 import { DATE_TIME_FORMAT_MIN } from "../../utils/constants";
-import "./Transport.css";
-import { map } from "ionicons/icons";
 
 interface TransportProps {
   routeTransportId: string;
@@ -20,6 +21,7 @@ interface TransportProps {
 const Transport = (): JSX.Element => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const management = useTypedSelector((state) => state.supervisionReducer);
 
@@ -27,9 +29,9 @@ const Transport = (): JSX.Element => {
     networkStatus: { isFailed = {} },
   } = management;
 
-  const { routeTransportId = "3" } = useParams<TransportProps>();
+  const { routeTransportId = "0" } = useParams<TransportProps>();
 
-  const { data: selectedRouteTransportDetails } = useQuery(
+  const { data: selectedRouteTransportDetail } = useQuery(
     ["getRouteTransport", routeTransportId],
     () => getRouteTransport(Number(routeTransportId), dispatch),
     {
@@ -37,7 +39,7 @@ const Transport = (): JSX.Element => {
     }
   );
 
-  const { data: selectedPermitDetails } = useQuery(
+  const { data: selectedPermitDetail } = useQuery(
     ["getPermitOfRouteTransport", routeTransportId],
     () => getPermitOfRouteTransport(Number(routeTransportId), dispatch),
     {
@@ -46,64 +48,83 @@ const Transport = (): JSX.Element => {
     }
   );
 
-  const noNetworkNoData = selectedRouteTransportDetails === undefined;
+  const { plannedDepartureTime, route } = selectedRouteTransportDetail || {};
+  const { permitNumber } = selectedPermitDetail || {};
+  const { name: routeName } = route || {};
 
-  const departureTime = selectedRouteTransportDetails?.plannedDepartureTime;
+  const headerText = `${moment(plannedDepartureTime).format(DATE_TIME_FORMAT_MIN)} - ${routeName}`;
+
+  const noNetworkNoData =
+    (isFailed.getRouteTransport && selectedRouteTransportDetail === undefined) ||
+    (isFailed.getPermitOfRouteTransport && selectedPermitDetail === undefined);
 
   return (
     <IonPage>
-      <Header title={t("transports.transport.header.title")} somethingFailed={isFailed.getRouteTransport} />
-      <IonContent class="ion-padding" color="light">
-        {noNetworkNoData || !selectedRouteTransportDetails || !selectedPermitDetails ? (
+      <Header title={headerText} somethingFailed={isFailed.getRouteTransport || isFailed.getPermitOfRouteTransport} />
+      <IonContent fullscreen color="light">
+        {noNetworkNoData ? (
           <NoNetworkNoData />
         ) : (
-          <IonGrid>
+          <IonGrid className="ion-no-padding" fixed>
             <IonRow>
-              <IonCol class="ion-text-end">
-                <IonText>
-                  {t("transports.transport.permitLabel")} {selectedPermitDetails.permitNumber} (pdf)
-                </IonText>
+              <IonCol size="12" className="whiteBackground">
+                <IonGrid className="ion-no-padding">
+                  <IonRow className="ion-margin">
+                    <IonCol size="12" size-sm="3" size-lg="2">
+                      <IonText className="headingText">{t("transports.transport.permitLabel")}</IonText>
+                    </IonCol>
+                    <IonCol size="12" size-sm="9" size-lg="10">
+                      <IonText>{permitNumber}</IonText>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
               </IonCol>
             </IonRow>
+
             <IonRow>
-              <IonCol>
-                <h3>
-                  <IonText>{`${moment(departureTime).format(DATE_TIME_FORMAT_MIN)}`}</IonText>
-                </h3>
+              <IonCol className="whiteBackground">
+                <IonGrid className="ion-no-padding">
+                  <IonRow className="ion-margin">
+                    <IonCol>
+                      <RouteAccordion route={route as IRoute} isPanelOpen />
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
               </IonCol>
             </IonRow>
+
             <IonRow>
-              <IonCol>
-                <IonText>
-                  {t("transports.transport.routeLabel")} {selectedRouteTransportDetails.route?.name}
-                </IonText>
+              <IonCol size="12" className="whiteBackground">
+                <IonGrid className="ion-no-padding">
+                  <IonRow className="ion-margin">
+                    <IonCol size="12" size-sm="4" size-lg="3">
+                      <IonText className="headingText">{t("transports.transport.transportStatusLabel")}</IonText>
+                    </IonCol>
+                    <IonCol size="12" size-sm="8" size-lg="9">
+                      <IonText>{t("transports.transport.transportStatusBoxLabel.notStarted")}</IonText>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
               </IonCol>
             </IonRow>
+
             <IonRow>
-              <IonCol size="3">
-                <IonButton color="#ffffff">
-                  <IonIcon slot="start" icon={map}></IonIcon>
-                  {t("transports.transport.mapButtonLabel")}
-                </IonButton>
-              </IonCol>
-              <IonCol>
-                <div>{selectedRouteTransportDetails.route?.departureAddress.streetAddress}</div>
-                <div>{selectedRouteTransportDetails.route?.arrivalAddress.streetAddress}</div>
-              </IonCol>
-            </IonRow>
-            <IonRow>
-              <IonCol class="ion-text-left">
-                <IonText>{t("transports.transport.transportStatusLabel")}</IonText>
-              </IonCol>
-            </IonRow>
-            <IonRow>
-              <IonCol class="ion-text-center">
-                <div className="statusBox">{t("transports.transport.transportStatusBoxLabel.notStarted")}</div>
-              </IonCol>
-            </IonRow>
-            <IonRow>
-              <IonCol class="ion-text-left">
-                <IonButton>{t("transports.transport.startDrivingButtonLabel")}</IonButton>
+              <IonCol size="12" className="whiteBackground">
+                <IonGrid className="ion-no-padding">
+                  <IonRow className="ion-margin">
+                    <IonCol size="12" size-sm="6" size-lg="5" className="ion-padding-end ion-padding-bottom">
+                      <IonButton color="primary" expand="block" size="large">
+                        {t("transports.transport.startDrivingButtonLabel")}
+                      </IonButton>
+                    </IonCol>
+                    <IonCol size="12" size-sm="4" size-lg="3" className="ion-padding-end ion-padding-bottom">
+                      <IonButton color="tertiary" expand="block" size="large" onClick={() => history.goBack()}>
+                        <IonIcon className="otherIcon" icon={arrowLeft} slot="start" />
+                        {t("common.buttons.back2")}
+                      </IonButton>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
               </IonCol>
             </IonRow>
           </IonGrid>
