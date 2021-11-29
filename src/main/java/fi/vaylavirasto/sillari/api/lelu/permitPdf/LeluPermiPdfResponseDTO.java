20fec1627c845a6c29c5091bd4c0542349ef0c7d
mustonenk <kalle.mustonen@cgi.com>
2021-11-29 10:11:18 +0200

    lupa pdf

--- src/main/java/fi/vaylavirasto/sillari/api/lelu/permitPdf/LeluPermiPdfResponseDTO.java
+++ src/main/java/fi/vaylavirasto/sillari/api/lelu/permitPdf/LeluPermiPdfResponseDTO.java
@@ -1,4 +1,13 @@
 package fi.vaylavirasto.sillari.api.lelu.permitPdf;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class LeluPermiPdfResponseDTO {
+    @Schema(description = "Permit ID in SillaRi", example = "737")
+    private Long permitId;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @Schema(description = "Response success message", example = "Lupa pdf ladattu")
+    private String message;
 }
