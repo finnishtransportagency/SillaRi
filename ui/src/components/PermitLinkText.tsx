1f57fba13c8d6772bdc8967d7e0077a198e26591
Anthony Brown <ant.brown@cgi.com>
2021-12-09 17:18:11 +0200

    Added components for displaying pdf links

---
+++ ui/src/components/PermitLinkText.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { IonText } from "@ionic/react";
+import IPermit from "../interfaces/IPermit";
+import PermitPdfPreview from "./PermitPdfPreview";
+
+interface PermitLinkTextProps {
+  permit: IPermit;
+}
+
+const PermitLinkText = ({ permit }: PermitLinkTextProps): JSX.Element => {
+  const [isPermitPdfOpen, setPermitPdfOpen] = useState<boolean>(false);
+  const { permitNumber, pdfObjectKey = "" } = permit || {};
+
+  return pdfObjectKey.length > 0 ? (
+    <>
+      <IonText className="linkText" onClick={() => setPermitPdfOpen(true)}>
+        {permitNumber}
+      </IonText>
+
+      {isPermitPdfOpen && <PermitPdfPreview pdfObjectKey={pdfObjectKey} isOpen={isPermitPdfOpen} setOpen={setPermitPdfOpen} />}
+    </>
+  ) : (
+    <IonText>{permitNumber}</IonText>
+  );
+};
+
+export default PermitLinkText;
