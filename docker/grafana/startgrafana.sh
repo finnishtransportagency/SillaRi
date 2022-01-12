7f9383b538fc7e65a201784c2c44e22a15f20928
Jari Piitulainen <jari.piitulainen@cgi.com>
2022-01-12 13:04:38 +0200

    prod environment

--- docker/grafana/startgrafana.sh
+++ docker/grafana/startgrafana.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 cat /var/ktv/hosts >> /etc/hosts
+if [ "$environment" == "prod" ] ; then
+    export grafana_root_uri=sillari.vaylapilvi.fi/grafana/
+    export grafana_domain=sillari.vaylapilvi.fi
+else
 if [ "$environment" == "dev" ] ; then
     export grafana_root_uri=sillaridev.testivaylapilvi.fi/grafana/
     export grafana_domain=sillaridev.testivaylapilvi.fi
