8bda1f0de45067f300266874863d6eadfe32facf
Jari Piitulainen <jari.piitulainen@cgi.com>
2022-01-18 11:35:18 +0200

    db.url from container env variables

--- src/main/java/fi/vaylavirasto/sillari/SillariApplication.java
+++ src/main/java/fi/vaylavirasto/sillari/SillariApplication.java
@@ -25,6 +25,8 @@ public class SillariApplication {
         logger.debug("application starting");
         logger.info("db.url = " + System.getenv("db.url"));
         logger.info("db.username = " + System.getenv("db.username"));
+        System.out.println("db.url = " + System.getenv("db.url"));
+        System.out.println("db.username = " + System.getenv("db.username"));
         SpringApplication.run(SillariApplication.class, args);
     }
     @Bean
