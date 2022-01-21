7be39291db79f70322222acf19d1a24cbc722828
Aki Hiisilä <aki.hiisila@cgi.com>
2022-01-21 10:48:17 +0200

    Handle fetching userdata response status 403 in ui.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -61,12 +61,17 @@ const queryClient = new QueryClient();
 const App: React.FC = () => {
   const [userData, setUserData] = useState<IUserData>();
   const [homePage, setHomePage] = useState<string>("/supervisions");
-  const [errorMsg, setErrorMsg] = useState<string>();
+  const [errorCode, setErrorCode] = useState<number>(0);
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const userDataResponse = await fetch(`${getOrigin()}/api/ui/userdata`);
+        /* Disabling caching to avoid getting stale user data. */
+        const headers = new Headers();
+        headers.append("pragma", "no-cache");
+        headers.append("cache-control", "no-store");
+
+        const userDataResponse = await fetch(`${getOrigin()}/api/ui/userdata`, { method: "GET", headers: headers });
 
         if (userDataResponse?.ok) {
           const responseData = await userDataResponse.json();
@@ -80,19 +85,25 @@ const App: React.FC = () => {
             }
             setUserData(responseData);
           } else {
-            setErrorMsg("Ei oikeutta SillaRi-sovelluksen käyttöön.");
+            /* Should never happen, since backend returns 403, if user does not have SillaRi roles. */
+            setErrorCode(1001);
           }
         } else {
           console.log(userDataResponse);
           if (userDataResponse.status === 401) {
-            setErrorMsg("401 - Access denied.");
+            /* Returned by Väylä access control. */
+            setErrorCode(401);
+          } else if (userDataResponse.status === 403) {
+            /* Returned by SillaRi backend if user does not have SillaRi roles. */
+            setErrorCode(403);
           } else {
-            setErrorMsg("Käsittelemätön virhetilanne 1.");
+            /* Properly handling other response code not handled yet. */
+            setErrorCode(1002);
           }
         }
       } catch (e) {
         console.log(e);
-        setErrorMsg("Käsittelemätön virhetilanne 2.");
+        setErrorCode(1003);
       }
     };
     fetchUserData();
@@ -115,22 +126,34 @@ const App: React.FC = () => {
     return false;
   };
 
+  const renderError = (code: number) => {
+    return (
+      <>
+        {code === 401 ? (
+          <div>
+            <h1>Pääsy estetty</h1>
+            <p>Kirjaudu sisään käyttääksesi sovellusta.</p>
+            <IonButton color="primary" expand="block" size="large" onClick={logoutFromApp}>
+              Kirjaudu sisään
+            </IonButton>
+          </div>
+        ) : code === 403 ? (
+          <div>
+            <h1>Pääsy estetty</h1>
+            <p>Sinulla ei ole oikeuksia käyttää SillaRi-sovellusta.</p>
+          </div>
+        ) : (
+          <div>Käsittelemätön virhetilanne: {code}</div>
+        )}
+      </>
+    );
+  };
+
   return (
     <QueryClientProvider client={queryClient}>
       <IonApp>
         {!userData ? (
-          <div>
-            {errorMsg ? (
-              <div>
-                <div>{errorMsg}</div>
-                <IonButton color="primary" expand="block" size="large" onClick={logoutFromApp}>
-                  Kirjaudu sisään
-                </IonButton>
-              </div>
-            ) : (
-              <div>Starting app...</div>
-            )}
-          </div>
+          <IonContent className="ion-padding">{errorCode ? <>{renderError(errorCode)}</> : <div>Starting app...</div>}</IonContent>
         ) : (
           <IonReactRouter>
             <SidebarMenu roles={userData.roles} />
