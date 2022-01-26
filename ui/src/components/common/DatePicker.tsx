b770c598e3218934d7b4a23ece4776c874017ced
Anthony Brown <ant.brown@cgi.com>
2022-01-26 11:14:20 +0200

    Changed date select to contain year, and multiple separator handling

--- ui/src/components/common/DatePicker.tsx
+++ ui/src/components/common/DatePicker.tsx
@@ -21,11 +21,27 @@ const validateInput = (inputValue: string, prevInputValue: string) => {
     return "";
   } else {
     // Check that the input value matches the format in the dropdown
-    // Allow the user to enter numbers, and add the separator automatically if missing
+    // Allow the user to enter numbers, and add the separators automatically if missing
     // The select dropdown filter will determine the final value, so no need to do date validation
-    const testValue = inputValue.length >= 3 && inputValue.indexOf(".") < 0 ? `${inputValue.substring(0, 2)}.${inputValue.substring(2)}` : inputValue;
-    const regex = /^\d{1,2}\.?\d{0,2}$/g;
-    return testValue.match(regex) ? testValue : prevInputValue;
+    const dotRegex = /\./g;
+    const dots = inputValue.match(dotRegex);
+    const firstDot = inputValue.indexOf(".");
+
+    // If the value contains the day and month, add a separator then the year, then return if valid
+    const testValue1 =
+      inputValue.length >= 6 && dots && dots.length === 1
+        ? `${inputValue.substring(0, firstDot)}.${inputValue.substring(firstDot + 1, firstDot + 3)}.${inputValue.substring(firstDot + 3)}`
+        : inputValue;
+    const regex1 = /^\d{1,2}\.\d{1,2}\.?\d{0,4}$/g;
+
+    if (testValue1.match(regex1)) {
+      return testValue1;
+    } else {
+      // If the value contains only the day, add a separator then the month, then return if valid, otherwise just return the input
+      const testValue2 = inputValue.length >= 3 && !dots ? `${inputValue.substring(0, 2)}.${inputValue.substring(2)}` : inputValue;
+      const regex2 = /^\d{1,2}\.?\d{0,2}$/g;
+      return testValue2.match(regex2) ? testValue2 : prevInputValue;
+    }
   }
 };
 
@@ -35,7 +51,7 @@ const DatePicker = ({ value, onChange }: DatePickerProps): JSX.Element => {
   return (
     <CustomSelect
       options={getDaysArray(min, max).map((date) => {
-        return { value: moment(date).format("YYYY-MM-DD"), label: moment(date).format("D.M") };
+        return { value: moment(date).format("YYYY-MM-DD"), label: moment(date).format("D.M.YYYY") };
       })}
       selectedValue={moment(value).format("YYYY-MM-DD")}
       onChange={(date) => {
