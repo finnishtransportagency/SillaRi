ba9ef233a1e949345d0862ab7a82a2258bad6e9b
mustonenk <Lncj6324Z>
2022-02-25 17:42:10 +0200

    FIM

--- src/main/java/fi/vaylavirasto/sillari/repositories/SupervisionRepository.java
+++ src/main/java/fi/vaylavirasto/sillari/repositories/SupervisionRepository.java
@@ -172,7 +172,7 @@ supervisionModel.getRouteBridgeId(),
             supervisionModel.setId(supervisionId);
 
             supervisionModel.getSupervisors().forEach(supervisorModel -> {
-                supervisorRepository.insertSupervisionSupervisor(ctx, supervisorModel.getId(), supervisorModel.getPriority(), supervisorModel.getUsername());
+                supervisorRepository.insertSupervisionSupervisor(ctx, supervisionModel.getId(), supervisorModel.getPriority(), supervisorModel.getUsername());
             });
 
             return supervisionId;
