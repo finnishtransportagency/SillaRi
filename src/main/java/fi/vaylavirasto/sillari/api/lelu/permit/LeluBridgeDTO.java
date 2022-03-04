f8fb09e9d5b3317bc986a4ac50d0222c6f73f29b
mustonenk <Lncj6324Z>
2022-03-04 13:41:51 +0200

    lelu changes; outcoming order number

--- src/main/java/fi/vaylavirasto/sillari/api/lelu/permit/LeluBridgeDTO.java
+++ src/main/java/fi/vaylavirasto/sillari/api/lelu/permit/LeluBridgeDTO.java
@@ -39,7 +39,7 @@ public class LeluBridgeDTO {
     @Schema(description = "Contractor y-tunnus from Harja. Might be missing", example = "1234567-8")
     private String contractBusinessIdentifier;
 
-    @Schema(description = "Number of crossing, starting from 1.", example = "1")
+    @Schema(description = "Order number of crossing, starting from 1.", example = "1")
     @NotBlank(message = "{transport.number.not.blank}")
     private Integer transportNumber;
 
