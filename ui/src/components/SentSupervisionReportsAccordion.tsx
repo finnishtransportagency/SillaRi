6966e67163f2bad6139a8af99f95b522f9bc4356
Noora Oksanen <noora.oksanen@cgi.com>
2022-03-24 10:08:11 +0200

    Add supervision info

--- ui/src/components/SentSupervisionReportsAccordion.tsx
+++ ui/src/components/SentSupervisionReportsAccordion.tsx
@@ -2,17 +2,20 @@ import React, { useEffect, useState } from "react";
 import CustomAccordion from "./common/CustomAccordion";
 import ISupervision from "../interfaces/ISupervision";
 import ISupervisionDay from "../interfaces/ISupervisionDay";
-import { groupSupervisionsBySignedDate } from "../utils/supervisionUtil";
-import { IonCol, IonGrid, IonRow, IonText } from "@ionic/react";
+import { getReportSignedTime, groupSupervisionsBySignedDate } from "../utils/supervisionUtil";
+import { IonCol, IonGrid, IonItem, IonLabel, IonRow, IonText } from "@ionic/react";
 import Moment from "react-moment";
-import { DATE_FORMAT } from "../utils/constants";
+import { DATE_FORMAT, DATE_TIME_FORMAT_MIN } from "../utils/constants";
 import "./SentSupervisionReportsAccordion.css";
+import { useTranslation } from "react-i18next";
+import moment from "moment";
 
 interface SentSupervisionReportsAccordionProps {
   sentSupervisions: ISupervision[];
 }
 
 const SentSupervisionReportsAccordion = ({ sentSupervisions }: SentSupervisionReportsAccordionProps): JSX.Element => {
+  const { t } = useTranslation();
   const [supervisionDays, setSupervisionDays] = useState<ISupervisionDay[]>([]);
 
   useEffect(() => {
@@ -36,7 +39,7 @@ const SentSupervisionReportsAccordion = ({ sentSupervisions }: SentSupervisionRe
             <IonGrid className="ion-no-padding">
               <IonRow>
                 <IonCol>
-                  <IonText className="headingText">
+                  <IonText className="headingBoldText">
                     <Moment format={DATE_FORMAT}>{day.date}</Moment>
                     {` (${day.supervisions.length})`}
                   </IonText>
@@ -55,15 +58,53 @@ const SentSupervisionReportsAccordion = ({ sentSupervisions }: SentSupervisionRe
                 const { permit } = route || {};
                 const { permitNumber } = permit || {};
                 const { tractorUnit = "" } = routeTransport || {};
+                const reportSignedTime = getReportSignedTime(supervision);
 
                 return (
-                  <IonGrid key={sKey}>
-                    <IonRow>
-                      <IonCol>
-                        <IonText className="headingText">{`${name}, ${identifier}, ${municipality}`}</IonText>
-                      </IonCol>
-                    </IonRow>
-                  </IonGrid>
+                  <IonItem key={sKey} fill="outline" lines="none" className="ion-padding-start ion-padding-end ion-margin-bottom">
+                    <IonGrid className="ion-no-padding ion-padding-top ion-padding-bottom">
+                      <IonRow className="ion-margin-bottom">
+                        <IonCol>
+                          <IonText className="headingBoldText">{`${name}, ${identifier}, ${municipality}`}</IonText>
+                        </IonCol>
+                      </IonRow>
+                      <IonRow>
+                        <IonCol size-md="4">
+                          <IonLabel className="headingText">{t("sendingList.transportPermit")}</IonLabel>
+                        </IonCol>
+                        <IonCol>{permitNumber}</IonCol>
+                      </IonRow>
+                      <IonRow>
+                        <IonCol size-md="4">
+                          <IonLabel className="headingText">{t("sendingList.tractorUnit")}</IonLabel>
+                        </IonCol>
+                        <IonCol>{tractorUnit}</IonCol>
+                      </IonRow>
+                      <IonRow>
+                        <IonCol size-md="4">
+                          <IonLabel className="headingText">{t("sendingList.supervisionStarted")}</IonLabel>
+                        </IonCol>
+                        <IonCol>{moment(startedTime).format(DATE_TIME_FORMAT_MIN)}</IonCol>
+                      </IonRow>
+                      <IonRow>
+                        <IonCol size-md="4">
+                          <IonLabel className="headingText">{t("sendingList.reportSent")}</IonLabel>
+                        </IonCol>
+                        <IonCol>{reportSignedTime ? moment(reportSignedTime).format(DATE_TIME_FORMAT_MIN) : ""}</IonCol>
+                      </IonRow>
+                      <IonRow>
+                        <IonCol size-md="4">
+                          <IonLabel className="headingText">{t("sendingList.removed")}</IonLabel>
+                        </IonCol>
+                        {/*TODO how do we determine the time?*/}
+                      </IonRow>
+                      <IonRow>
+                        <IonCol>
+                          <IonLabel>{t("sendingList.report")}</IonLabel>
+                        </IonCol>
+                      </IonRow>
+                    </IonGrid>
+                  </IonItem>
                 );
               })}
             </div>
