54319c829d7f26e410a6c433e3d5be4dd122e9b3
Noora Oksanen <noora.oksanen@cgi.com>
2022-04-04 09:45:28 +0300

    Add next supervision time for transport

--- ui/src/components/TransportCard.tsx
+++ ui/src/components/TransportCard.tsx
@@ -3,8 +3,9 @@ import { IonItem, IonLabel, IonText } from "@ionic/react";
 import IRouteTransport from "../interfaces/IRouteTransport";
 import Moment from "react-moment";
 import arrowRight from "../theme/icons/arrow-right.svg";
-import { DATE_TIME_FORMAT_MIN, TransportStatus } from "../utils/constants";
+import { DATE_TIME_FORMAT_MIN, TIME_FORMAT_MIN, TransportStatus } from "../utils/constants";
 import { useTranslation } from "react-i18next";
+import { getNextPlannedSupervisionTime } from "../utils/supervisionUtil";
 
 interface TransportCardProps {
   transport: IRouteTransport;
@@ -13,13 +14,15 @@ interface TransportCardProps {
 const TransportCard = ({ transport }: TransportCardProps): JSX.Element => {
   const { t } = useTranslation();
 
-  const { id: routeTransportId, currentStatus, departureTime, plannedDepartureTime, route } = transport || {};
+  const { id: routeTransportId, currentStatus, departureTime, plannedDepartureTime, route, supervisions = [] } = transport || {};
 
   const { permit, name: routeName } = route || {};
   const { permitNumber } = permit || {};
 
   const { status } = currentStatus || {};
   const transportDeparted = status && status !== TransportStatus.PLANNED;
+
+  const nextSupervisionTime = getNextPlannedSupervisionTime(supervisions);
 
   return (
     <IonItem className="itemIcon iconLink" detail detailIcon={arrowRight} routerLink={`/routetransportdetail/${routeTransportId}`}>
@@ -34,6 +37,15 @@ const TransportCard = ({ transport }: TransportCardProps): JSX.Element => {
         <IonLabel>
           <small>{`${t("companyTransports.transportPermit")} ${permitNumber}`}</small>
         </IonLabel>
+        {nextSupervisionTime && (
+          <IonLabel>
+            <small>
+              <IonText>{`${t("companyTransports.nextSupervision")} `}</IonText>
+              <Moment format={TIME_FORMAT_MIN}>{nextSupervisionTime}</Moment>
+              <IonText>{` (${t("companyTransports.estimate")})`}</IonText>
+            </small>
+          </IonLabel>
+        )}
       </IonLabel>
     </IonItem>
   );
