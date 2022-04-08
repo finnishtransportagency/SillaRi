c2ff8e677df892e2fa6e0913e7a9964be5e67712
Aki Hiisilä <aki.hiisila@cgi.com>
2022-04-08 11:04:06 +0300

    Get ui language from persistent storage before rendering app

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -13,33 +13,32 @@ import { Storage } from "@capacitor/storage";
 import { changeLanguage } from "i18next";
 
 const rootElement = document.getElementById("root");
+const root = createRoot(rootElement as Element);
 
-const setUiLang = async () => {
+(async () => {
   const lang = await Storage.get({ key: "ui_lang" });
   if (lang.value) {
     changeLanguage(lang.value);
   }
-};
-setUiLang();
 
-const root = createRoot(rootElement as Element);
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+  // ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://cra.link/PWA
+  serviceWorkerRegistration.register();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
 
-// Call the element loader after the app has been rendered the first time
-defineCustomElements(window);
+  // Call the element loader after the app has been rendered the first time
+  defineCustomElements(window);
+})();
