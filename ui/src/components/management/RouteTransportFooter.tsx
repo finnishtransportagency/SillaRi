1878c4bde63d1486950fc122d537b71916ed5191
Noora Oksanen <noora.oksanen@cgi.com>
2022-04-20 15:03:05 +0300

    Footer styling

--- ui/src/components/management/RouteTransportFooter.tsx
+++ ui/src/components/management/RouteTransportFooter.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { IonButton, IonCol, IonFooter, IonRow, IonText, IonToolbar } from "@ionic/react";
+import { IonButton, IonCol, IonFooter, IonGrid, IonRow, IonText, IonToolbar } from "@ionic/react";
 import { useHistory } from "react-router-dom";
 
 interface RouteTransportFooterProps {
@@ -27,38 +27,40 @@ const RouteTransportFooter = ({
 
   return (
     <IonFooter>
-      <IonToolbar>
-        {isEditable && (
-          <IonRow className="ion-margin ion-justify-content-end">
-            {!!routeTransportId && routeTransportId > 0 && (
-              <IonCol size="12" size-sm className="ion-padding-start ion-padding-bottom ion-text-center">
-                <IonButton color="tertiary" expand="block" size="large" disabled={deleteDisabled} onClick={() => deleteTransport()}>
-                  <IonText>{t("management.transportDetail.buttons.deleteTransport")}</IonText>
+      <IonToolbar color="light">
+        <IonGrid fixed>
+          {isEditable && (
+            <IonRow className="ion-justify-content-end">
+              {!!routeTransportId && routeTransportId > 0 && (
+                <IonCol size-sm className="ion-text-center">
+                  <IonButton color="tertiary" expand="block" size="large" disabled={deleteDisabled} onClick={() => deleteTransport()}>
+                    <IonText>{t("management.transportDetail.buttons.deleteTransport")}</IonText>
+                  </IonButton>
+                </IonCol>
+              )}
+              <IonCol size-sm className="ion-text-center">
+                <IonButton color="secondary" expand="block" size="large" disabled={cancelDisabled} onClick={() => history.goBack()}>
+                  <IonText>{t("common.buttons.cancel")}</IonText>
                 </IonButton>
               </IonCol>
-            )}
-            <IonCol size="12" size-sm className="ion-padding-start ion-padding-bottom ion-text-center">
-              <IonButton color="secondary" expand="block" size="large" disabled={cancelDisabled} onClick={() => history.goBack()}>
-                <IonText>{t("common.buttons.cancel")}</IonText>
-              </IonButton>
-            </IonCol>
-            <IonCol size="12" size-sm className="ion-padding-start ion-padding-bottom ion-text-center">
-              <IonButton color="primary" expand="block" size="large" disabled={saveDisabled} onClick={() => saveTransport()}>
-                <IonText>{t("common.buttons.save")}</IonText>
-              </IonButton>
-            </IonCol>
-          </IonRow>
-        )}
+              <IonCol size-sm className="ion-text-center">
+                <IonButton color="primary" expand="block" size="large" disabled={saveDisabled} onClick={() => saveTransport()}>
+                  <IonText>{t("common.buttons.save")}</IonText>
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          )}
 
-        {!isEditable && (
-          <IonRow className="ion-margin ion-justify-content-end">
-            <IonCol size="12" size-sm className="ion-padding-start ion-padding-bottom ion-text-center">
-              <IonButton color="primary" expand="block" size="large" disabled={cancelDisabled} onClick={() => history.goBack()}>
-                <IonText>{t("common.buttons.back2")}</IonText>
-              </IonButton>
-            </IonCol>
-          </IonRow>
-        )}
+          {!isEditable && (
+            <IonRow className="ion-justify-content-end">
+              <IonCol size-sm className="ion-text-center">
+                <IonButton color="primary" expand="block" size="large" disabled={cancelDisabled} onClick={() => history.goBack()}>
+                  <IonText>{t("common.buttons.back2")}</IonText>
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          )}
+        </IonGrid>
       </IonToolbar>
     </IonFooter>
   );
