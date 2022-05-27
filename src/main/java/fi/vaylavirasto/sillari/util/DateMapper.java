5431a11199851024a9e8a7a8688acb34be5bb8aa
Noora Oksanen <noora.oksanen@cgi.com>
2022-05-27 10:07:59 +0300

    Null checks to DateMapper

--- src/main/java/fi/vaylavirasto/sillari/util/DateMapper.java
+++ src/main/java/fi/vaylavirasto/sillari/util/DateMapper.java
@@ -9,21 +9,33 @@ import java.time.format.DateTimeFormatter;
 public class DateMapper {
 
     public static OffsetDateTime localDateToOffsetDate(LocalDateTime localDateTime) {
+        if (localDateTime == null) {
+            return null;
+        }
         final ZoneId zone = ZoneId.of("Europe/Helsinki");
         ZoneOffset zoneOffSet = zone.getRules().getOffset(localDateTime);
         return localDateTime.atOffset(zoneOffSet);
     }
 
     public static LocalDateTime offsetDateToLocalDate(OffsetDateTime offsetDateTime) {
+        if (offsetDateTime == null) {
+            return null;
+        }
         return offsetDateTime.toLocalDateTime();
     }
 
     public static LocalDateTime stringToLocalDate(String dateTime) {
+        if (dateTime == null) {
+            return null;
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
         return LocalDateTime.parse(dateTime, formatter);
     }
 
     public static OffsetDateTime stringToOffsetDate(String dateTime) {
+        if (dateTime == null) {
+            return null;
+        }
         LocalDateTime localDateTime = stringToLocalDate(dateTime);
         return localDateToOffsetDate(localDateTime);
     }
