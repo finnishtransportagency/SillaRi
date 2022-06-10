b3fefd0632b6d78453218d8f7c34f6cc24afa294
Noora Oksanen <noora.oksanen@cgi.com>
2022-06-10 14:33:20 +0300

    Insert route_transport_number rows for test data

--- src/main/resources/db/migration/V94__route_transport_number.sql
+++ src/main/resources/db/migration/V94__route_transport_number.sql
@@ -17,3 +17,26 @@ CREATE TRIGGER route_transport_count_row_updated
     BEFORE UPDATE ON sillari.route_transport_count
     FOR EACH ROW
 EXECUTE PROCEDURE update_row_updated_time();
+
+
+-- Add route_transport_number rows for each routes and max transport_number
+do $$
+    declare rt record;
+    begin
+        for rt in select r.id, r.transport_count
+                  from sillari.route r
+            loop
+                for c in 1..rt.transport_count loop
+                        insert into sillari.route_transport_count (route_id, count, used) values (rt.id, c, false);
+                    end loop;
+            end loop;
+    end; $$;
+
+-- Update route_transport_number as used when corresponding route_transport has been created
+update sillari.route_transport_count rtc
+set route_transport_id = (
+    select rt.id from route_transport rt
+    where rt.route_id = rtc.route_id and rt.transport_number = rtc.count
+);
+
+update sillari.route_transport_count rtn set used = true where rtn.route_transport_id is not null;
