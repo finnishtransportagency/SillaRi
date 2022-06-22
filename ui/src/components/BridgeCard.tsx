9f0152245833418d8d792cc188ed017ca7c43723
Noora Oksanen <noora.oksanen@cgi.com>
2022-06-22 16:01:55 +0300

    Add password buttons

--- ui/src/components/BridgeCard.tsx
+++ ui/src/components/BridgeCard.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Moment from "react-moment";
-import { IonItem, IonLabel, IonText } from "@ionic/react";
-import arrowRight from "../theme/icons/arrow-right.svg";
+import { IonButton, IonCol, IonGrid, IonIcon, IonItem, IonLabel, IonRow, IonText } from "@ionic/react";
 import { SupervisionStatus, TIME_FORMAT_MIN } from "../utils/constants";
 import ISupervision from "../interfaces/ISupervision";
 import { useTranslation } from "react-i18next";
@@ -9,6 +8,7 @@ import IRouteTransport from "../interfaces/IRouteTransport";
 import { useHistory } from "react-router-dom";
 import { actions } from "../store/rootSlice";
 import { useDispatch } from "react-redux";
+import lock from "../theme/icons/lock_closed_white.svg";
 
 interface BridgeCardProps {
   supervision: ISupervision;
@@ -35,22 +35,39 @@ const BridgeCard = ({ supervision, routeTransport, supervisionListType }: Bridge
   };
 
   return (
-    <IonItem className="itemIcon iconLink quarter-margin-bottom" lines="full" detail detailIcon={arrowRight} onClick={() => navigateToBridgeDetail()}>
-      <IonLabel>
-        <IonLabel className="headingText">
-          <Moment format={TIME_FORMAT_MIN}>{statusPlanned ? plannedTime : startedTime}</Moment>
-          {statusPlanned && <IonText>{` (${t("bridgeCard.estimate")})`}</IonText>}
-        </IonLabel>
-        <IonLabel className="headingText">{name}</IonLabel>
-        <IonLabel>
-          <small>
-            {`${identifier}, ${municipality}`}
-            {routeTransport && (
-              <IonText>{` | ${t("bridgeCard.tractorUnit")} ${tractorUnit ? tractorUnit.toUpperCase() : tractorUnitMissing}`}</IonText>
-            )}
-          </small>
-        </IonLabel>
-      </IonLabel>
+    <IonItem className="quarter-margin-bottom" lines="full" onClick={() => navigateToBridgeDetail()}>
+      <IonGrid className="ion-no-margin ion-no-padding">
+        <IonRow className="ion-margin-vertical ion-align-items-center">
+          <IonCol size="10">
+            <IonLabel>
+              <IonLabel className="headingText">
+                <Moment format={TIME_FORMAT_MIN}>{statusPlanned ? plannedTime : startedTime}</Moment>
+                {statusPlanned && <IonText>{` (${t("bridgeCard.estimate")})`}</IonText>}
+              </IonLabel>
+              <IonLabel className="headingText">{name}</IonLabel>
+              <IonLabel>
+                <small>
+                  {`${identifier}, ${municipality}`}
+                  {routeTransport && (
+                    <IonText>{` | ${t("bridgeCard.tractorUnit")} ${tractorUnit ? tractorUnit.toUpperCase() : tractorUnitMissing}`}</IonText>
+                  )}
+                </small>
+              </IonLabel>
+            </IonLabel>
+          </IonCol>
+          <IonCol size="1">
+            <IonButton
+              color="secondary"
+              className="passwordButton"
+              onClick={() => {
+                console.log("Password button clicked");
+              }}
+            >
+              <IonIcon className="otherIcon" icon={lock} />
+            </IonButton>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
     </IonItem>
   );
 };
--- ui/src/components/TransportCard.tsx
+++ ui/src/components/TransportCard.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { IonItem, IonLabel, IonText } from "@ionic/react";
+import { IonButton, IonCol, IonGrid, IonIcon, IonItem, IonLabel, IonRow, IonText } from "@ionic/react";
 import IRouteTransport from "../interfaces/IRouteTransport";
 import Moment from "react-moment";
-import arrowRight from "../theme/icons/arrow-right.svg";
 import { DATE_FORMAT, TIME_FORMAT_MIN, TransportStatus } from "../utils/constants";
 import { useTranslation } from "react-i18next";
 import { getNextPlannedSupervisionTime } from "../utils/supervisionUtil";
 import "./TransportCard.css";
+import lock from "../theme/icons/lock_closed_white.svg";
 
 interface TransportCardProps {
   transport: IRouteTransport;
@@ -36,35 +36,50 @@ const TransportCard = ({ transport }: TransportCardProps): JSX.Element => {
 
   return (
     <IonItem
-      className={`itemIcon iconLink ion-margin-horizontal quarter-margin-bottom ${transportDeparted ? "departedTransport" : ""}`}
+      className={`ion-margin-horizontal quarter-margin-bottom ${transportDeparted ? "departedTransport" : ""}`}
       lines="full"
       color={transportDeparted ? undefined : "light"}
-      detail
-      detailIcon={arrowRight}
       routerLink={`/routetransportdetail/${routeTransportId}`}
     >
-      <IonLabel color={transportDeparted ? undefined : "dark"}>
-        <IonLabel className={transportDeparted ? "headingText" : "headingText upcomingTransport"}>
-          <Moment format={DATE_FORMAT}>{transportDeparted ? departureTime : plannedDepartureTime}</Moment>
-          <IonText>{tractorUnit ? ` | ${tractorUnit.toUpperCase()}` : ""}</IonText>
-        </IonLabel>
-        <IonLabel>
-          <small>{routeName}</small>
-        </IonLabel>
-        <IonLabel>
-          <small>{`${t("companyTransports.transportPermit")} ${permitNumber}`}</small>
-          {transportNumber && <small>{` | ${t("companyTransports.transportNumber")}: ${transportNumber}`}</small>}
-        </IonLabel>
-        {nextSupervisionTime && (
-          <IonLabel>
-            <small>
-              <IonText>{`${t("companyTransports.nextSupervision")} `}</IonText>
-              <Moment format={TIME_FORMAT_MIN}>{nextSupervisionTime}</Moment>
-              <IonText>{` (${t("companyTransports.estimate")})`}</IonText>
-            </small>
-          </IonLabel>
-        )}
-      </IonLabel>
+      <IonGrid className="ion-no-margin ion-no-padding">
+        <IonRow className="ion-margin-vertical ion-align-items-center">
+          <IonCol size="10">
+            <IonLabel color={transportDeparted ? undefined : "dark"}>
+              <IonLabel className={transportDeparted ? "headingText" : "headingText upcomingTransport"}>
+                <Moment format={DATE_FORMAT}>{transportDeparted ? departureTime : plannedDepartureTime}</Moment>
+                <IonText>{tractorUnit ? ` | ${tractorUnit.toUpperCase()}` : ""}</IonText>
+              </IonLabel>
+              <IonLabel>
+                <small>{routeName}</small>
+              </IonLabel>
+              <IonLabel>
+                <small>{`${t("companyTransports.transportPermit")} ${permitNumber}`}</small>
+                {transportNumber && <small>{` | ${t("companyTransports.transportNumber")}: ${transportNumber}`}</small>}
+              </IonLabel>
+              {nextSupervisionTime && (
+                <IonLabel>
+                  <small>
+                    <IonText>{`${t("companyTransports.nextSupervision")} `}</IonText>
+                    <Moment format={TIME_FORMAT_MIN}>{nextSupervisionTime}</Moment>
+                    <IonText>{` (${t("companyTransports.estimate")})`}</IonText>
+                  </small>
+                </IonLabel>
+              )}
+            </IonLabel>
+          </IonCol>
+          <IonCol size="1">
+            <IonButton
+              color="secondary"
+              className="passwordButton"
+              onClick={() => {
+                console.log("Password button clicked");
+              }}
+            >
+              <IonIcon className="otherIcon" icon={lock} />
+            </IonButton>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
     </IonItem>
   );
 };
