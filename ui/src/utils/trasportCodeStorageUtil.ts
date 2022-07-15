48a89dd5ed599c4955dd1fca38a4e4e91701f888
mustonenk <kalle.mustonen@cgi.com>
2022-07-15 11:31:04 +0300

    obsolete passwords remove from storage

--- ui/src/utils/trasportCodeStorageUtil.ts
+++ ui/src/utils/trasportCodeStorageUtil.ts
@@ -11,10 +11,25 @@ export const formatDate = (date: Date): string => {
   return date.toISOString().slice(0, 10);
 };
 
+export const configureStorageForAll = async () => {
+  await Storage.configure({ group: TRANSPORT_CODE_STORAGE_GROUP });
+};
+
+export const configureStorageForDay = async (day: Date) => {
+  const dayString = formatDate(new Date());
+  await Storage.configure({ group: TRANSPORT_CODE_STORAGE_GROUP + "." + dayString });
+};
+
+export const configureStorageForToday = async () => {
+  await configureStorageForDay(new Date());
+};
+
+export const configureStorageForDaysAgo = async (days: number) => {
+  await configureStorageForDay(getPastDate(days));
+};
+
 export const savePasswordToStorage = async (username: string, id: number, password: string, type: SupervisionListType) => {
-  const today = formatDate(new Date());
-  console.log();
-  await Storage.configure({ group: TRANSPORT_CODE_STORAGE_GROUP + today });
+  await configureStorageForToday();
   return Storage.set({
     // SILLARI_TRANSCODE + username + TRANSPORT/BRIDGE + routeTransportId/supervisionId
     key: constructStorageKey(username, type, id),
@@ -23,31 +38,27 @@ export const savePasswordToStorage = async (username: string, id: number, passwo
   });
 };
 
-export const getPastDate = (daysAgo: number): string => {
+export const getPastDate = (daysAgo: number): Date => {
   const date = new Date();
   date.setDate(date.getDate() - daysAgo);
-  return formatDate(date);
+  return date;
 };
 
-export const getPasswordFromStorage = async (username: string, type: SupervisionListType, id: number) => {
-  const today = formatDate(new Date());
-  console.log("hello1" + type);
+export const getPasswordFromStorage = async (username: string, type: SupervisionListType, id: number): Promise<string | null> => {
+  //we can get all transcodes cause obsolete have been removed
+  await configureStorageForAll();
   const keys = await Storage.keys();
   console.log(keys);
-  await Storage.configure({ group: TRANSPORT_CODE_STORAGE_GROUP + today });
-  const keys2 = await Storage.keys();
-  console.log(keys2);
   const transportCodeStorageKey = `${username}_${SupervisionListType.TRANSPORT}_${id}`;
   const transportCode = await Storage.get({ key: transportCodeStorageKey });
+  return transportCode.value;
 };
 
 //get from storage password from TRANSPORT_CODE_STORAGE_LIFE_DAYS (3) past days
 export const getNonObsoletePasswords = async () => {
   const nonObsoleteTransportCodes = [];
   for (let n = 0; n < TRANSPORT_CODE_STORAGE_LIFE_DAYS; n++) {
-    const date = getPastDate(n);
-    console.log("Date:" + date);
-    await Storage.configure({ group: TRANSPORT_CODE_STORAGE_GROUP + date });
+    await configureStorageForDaysAgo(n);
     const keysFromDate = await Storage.keys();
     console.log("keysFromDate:");
     console.log(keysFromDate);
@@ -59,24 +70,14 @@ export const getNonObsoletePasswords = async () => {
 };
 
 export const removeObsoletePasswords = async () => {
-  await Storage.configure({ group: "hello" });
-  const keysToRemove1 = await Storage.keys();
-  console.log(keysToRemove1);
   const nonObsoleteTransportCodes: string[] = await getNonObsoletePasswords();
   console.log("nonObsoleteTransportCodes:");
   console.log(nonObsoleteTransportCodes);
   console.log(Storage);
-  await Storage.configure({ group: undefined });
+  await configureStorageForAll();
   console.log(Storage);
   const keysToRemove = await Storage.keys();
   console.log("all keys:");
-  console.log(keysToRemove);
-
-  //dont remove non related stuff from storage
-  keysToRemove.keys.filter((key: string) => {
-    return key.includes(TRANSPORT_CODE_PREFIX);
-  });
-  console.log("all keys, unrelated removed:");
   console.log(keysToRemove);
 
   //dont remove keys that are in the new keys
