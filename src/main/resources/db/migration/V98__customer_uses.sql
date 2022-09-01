ebef5044a6b60dccbbac771446416e0a32255ba8
mustonenk <kalle.mustonen@cgi.com>
2022-09-01 13:13:48 +0300

    customer uses sillari -field in db

--- src/main/resources/db/migration/V98__customer_uses.sql
+++ src/main/resources/db/migration/V98__customer_uses.sql
@@ -1,2 +1,2 @@
-alter table sillari.permit add column if not exists customer_uses_sillari boolean;
-comment on column sillari.route.ordinal is 'Does customer company use SillaRi to plan transports';
+alter table sillari.permit add column if not exists customer_uses_sillari boolean DEFAULT null;
+comment on column sillari.permit.customer_uses_sillari is 'Does customer company use SillaRi to plan transports';
