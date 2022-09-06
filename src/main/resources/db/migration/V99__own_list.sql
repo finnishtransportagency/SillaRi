fd3412378cb9a336f3855fae8b4ec4e9983c589a
mustonenk <kalle.mustonen@cgi.com>
2022-09-06 14:21:31 +0300

    own list sql indexes

--- src/main/resources/db/migration/V99__own_list.sql
+++ src/main/resources/db/migration/V99__own_list.sql
@@ -7,3 +7,5 @@ CREATE TABLE IF NOT EXISTS sillari.own_list
     PRIMARY KEY (id),
     CONSTRAINT own_list_supervision_id_fkey FOREIGN KEY (supervision_id) REFERENCES supervision (id) DEFERRABLE
 );
+create index if not exists own_list_supervision_id on sillari.own_list (supervision_id);
+create index if not exists own_list_contract_business_id on sillari.own_list (contract_business_id);
