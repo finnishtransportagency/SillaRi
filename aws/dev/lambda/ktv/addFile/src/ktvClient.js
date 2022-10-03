98c6cb260cb1c704f942432087834019538e5714
mustonenk <kalle.mustonen@cgi.com>
2022-10-03 17:58:44 +0300

    use real bridges is ktv lambda

--- aws/dev/lambda/ktv/addFile/src/ktvClient.js
+++ aws/dev/lambda/ktv/addFile/src/ktvClient.js
@@ -1,106 +1,121 @@
-const https = require('https');
+const https = require("https");
 
 module.exports = {
-    postFileToKVT: function(objectData, metadata, apiKey, imageKey) {
+  postFileToKVT: function (objectData, metadata, apiKey, imageKey) {
+    return new Promise((resolve, reject) => {
+      // Sillarin tunnisteavaruus on 22 ja kuvatyyppi 2101.
+      const ID_SPACE = 22;
+      const PIC_TYPE = 2101;
 
-        return new Promise((resolve, reject) => {
+      const {
+        bridgeidentifier,
+        bridgename,
+        bridgeoid,
+        createdtime,
+        filename,
+        objectidentifier,
+        permitnumber,
+        roadaddress,
+        supervisionexceptional,
+        supervisionfinishedtime,
+        supervisionid,
+        supervisionstartedtime,
+        x_coord,
+        y_coord,
+      } = metadata;
 
-            // Sillarin tunnisteavaruus on 22 ja kuvatyyppi 2101.
-            const ID_SPACE = 22;
-            const PIC_TYPE = 2101;
+      let bridgeProcessedName = "";
+      if (bridgename) {
+        const decodedName = decodeURIComponent(bridgename);
+        // decodeURIComponent does not handle '+' encoded in Java
+        bridgeProcessedName = decodedName.replace(/\+/g, " ");
+      }
 
+      console.log("bridgeidentifier: " + bridgeidentifier);
+      console.log("bridgename: " + bridgename);
+      console.log("bridgeProcessedName: " + bridgeProcessedName);
+      console.log("bridgeoid: " + bridgeoid);
+      console.log("createdtime: " + createdtime);
+      console.log("filename: " + filename);
+      console.log("objectidentifier: " + objectidentifier);
+      console.log("permitnumber: " + permitnumber);
+      console.log("roadaddress: " + roadaddress);
+      console.log("supervisionexceptional: " + supervisionexceptional);
+      console.log("supervisionfinishedtime: " + supervisionfinishedtime);
+      console.log("supervisionid: " + supervisionid);
+      console.log("supervisionstartedtime: " + supervisionstartedtime);
+      console.log("x_coord: " + x_coord);
+      console.log("y_coord: " + y_coord);
 
-            const { bridgeidentifier, bridgename, bridgeoid, createdtime, filename, objectidentifier, permitnumber, roadaddress, supervisionexceptional, supervisionfinishedtime, supervisionid, supervisionstartedtime, x_coord, y_coord } = metadata;
+      const roadAddressParts = roadaddress ? roadaddress.split("-") : [];
+      console.log(roadAddressParts);
 
-            let bridgeProcessedName = '';
-            if (bridgename) {
-                const decodedName = decodeURIComponent(bridgename);
-                // decodeURIComponent does not handle '+' encoded in Java
-                bridgeProcessedName = decodedName.replace(/\+/g, ' ');
-            }
+      // supervisionexceptional has been converted from Boolean to string - it can be null/undefined, "true" or "false"
+      let isExceptional = null;
+      if (supervisionexceptional === "true") {
+        isExceptional = true;
+      } else if (supervisionexceptional === "false") {
+        isExceptional = false;
+      }
 
-            console.log("bridgeidentifier: " + bridgeidentifier);
-            console.log("bridgename: " + bridgename);
-            console.log("bridgeProcessedName: " + bridgeProcessedName);
-            console.log("bridgeoid: " + bridgeoid);
-            console.log("createdtime: " + createdtime);
-            console.log("filename: " + filename);
-            console.log("objectidentifier: " + objectidentifier);
-            console.log("permitnumber: " + permitnumber);
-            console.log("roadaddress: " + roadaddress);
-            console.log("supervisionexceptional: " + supervisionexceptional);
-            console.log("supervisionfinishedtime: " + supervisionfinishedtime);
-            console.log("supervisionid: " + supervisionid);
-            console.log("supervisionstartedtime: " + supervisionstartedtime);
-            console.log("x_coord: " + x_coord);
-            console.log("y_coord: " + y_coord);
+      // TODO post supervision metadata how?
+      const postData = JSON.stringify({
+        data: [
+          {
+            tunnisteavaruus: ID_SPACE,
+            tunniste: objectidentifier,
+            kuvatyyppi: PIC_TYPE,
+            nimi: filename,
+            image: objectData,
+            tie: roadAddressParts[0] || null,
+            tieosa: roadAddressParts[1] || null,
+            etaisyys: roadAddressParts[2] || null,
+            ajorata: roadAddressParts[3] || null,
+            xkoordinaatti: x_coord,
+            ykoordinaatti: y_coord,
+            kuljetuslupanumero: permitnumber,
+            ylitys_aloitusaika: supervisionstartedtime,
+            ylitys_paattymisaika: supervisionfinishedtime || null, // Is usually undefined for images
+            sillantunnus: bridgeidentifier,
+            //sillantunnus: 3, // TODO change after KTV has updated their end, now it has to be number 1-10. This goes to location on ktv map near Loviisa
+            valvontatunnus: supervisionid,
+            poikkeusjarjestely: isExceptional,
+          },
+        ],
+      });
 
-            const roadAddressParts = roadaddress ? roadaddress.split('-') : [];
-            console.log(roadAddressParts);
+      console.log("postData", postData);
 
-            // supervisionexceptional has been converted from Boolean to string - it can be null/undefined, "true" or "false"
-            let isExceptional = null;
-            if (supervisionexceptional === "true") {
-                isExceptional = true;
-            } else if (supervisionexceptional === "false") {
-                isExceptional = false;
-            }
+      const options = {
+        hostname: process.env.KTV_HOSTNAME,
+        //path: '/ktv/api/public/KTJLisaaKuvia',
+        path: "/ktv/api/ktv/KTJLisaaKuvia",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": apiKey,
+        },
+      };
 
-            // TODO post supervision metadata how?
-            const postData = JSON.stringify({
-                data: [{
-                    tunnisteavaruus: ID_SPACE,
-                    tunniste: objectidentifier,
-                    kuvatyyppi: PIC_TYPE,
-                    nimi: filename,
-                    image: objectData,
-                    tie: roadAddressParts[0] || null,
-                    tieosa: roadAddressParts[1] || null,
-                    etaisyys: roadAddressParts[2] || null,
-                    ajorata: roadAddressParts[3] || null,
-                    xkoordinaatti: x_coord,
-                    ykoordinaatti: y_coord,
-                    kuljetuslupanumero: permitnumber,
-                    ylitys_aloitusaika: supervisionstartedtime,
-                    ylitys_paattymisaika: supervisionfinishedtime || null, // Is usually undefined for images
-                    // sillantunnus: bridgeidentifier,
-                    sillantunnus: 3, // TODO change after KTV has updated their end, now it has to be number 1-10. This goes to location on ktv map near Loviisa
-                    valvontatunnus: supervisionid,
-                    poikkeusjarjestely: isExceptional,
-                }]
-            });
+      const req = https.request(options, (res) => {
+        let body = "";
+        console.log("statusCode", res.statusCode);
 
-            const options = {
-                hostname: process.env.KTV_HOSTNAME,
-                path: '/ktv/api/public/KTJLisaaKuvia',
-                // path: '/ktv/api/ktv/KTJLisaaKuvia',
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-api-key': apiKey
-                }
-            };
+        res.setEncoding("utf8");
+        res.on("data", (chunk) => (body += chunk));
 
-            const req = https.request(options, res => {
-                let body = '';
-                console.log('statusCode', res.statusCode);
-
-                res.setEncoding('utf8');
-                res.on('data', (chunk) => body += chunk);
-
-                res.on('end', () => {
-                    console.log('Successfully processed HTTPS response');
-                    console.log('RES body', body);
-                });
-            });
-
-            req.on('error', error => {
-                console.error(error)
-            });
-
-            req.write(postData);
-            req.end();
-
+        res.on("end", () => {
+          console.log("Successfully processed HTTPS response");
+          console.log("RES body", body);
         });
-    }
+      });
+
+      req.on("error", (error) => {
+        console.error(error);
+      });
+
+      req.write(postData);
+      req.end();
+    });
+  },
 };
