f01d5d5a1a5dfc72ae1ea4fbbf5011f5b3940891
mustonenk <kalle.mustonen@cgi.com>
2022-10-14 13:18:54 +0300

    debug auth list

--- src/main/java/fi/vaylavirasto/sillari/auth/JwtRequestFilter.java
+++ src/main/java/fi/vaylavirasto/sillari/auth/JwtRequestFilter.java
@@ -164,6 +164,8 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                         authorityList.add(SillariRole.fromString("SILLARI_KULJETTAJA"));
                     }
 
+                    logger.debug("authorityList " + authorityList);
+
                     SillariUser userDetails = new SillariUser(userNameDetail, authorityList);
 
                     userDetails.setFirstName(firstName);
