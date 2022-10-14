e06af70217442a5bed10830e680a01cd1c3ce677
mustonenk <kalle.mustonen@cgi.com>
2022-10-14 12:00:23 +0300

    get ytunnus from lo and losv uid

--- src/test/java/fi/vaylavirasto/sillari/auth/JwtRequestFilterTest.java
+++ src/test/java/fi/vaylavirasto/sillari/auth/JwtRequestFilterTest.java
@@ -20,4 +20,24 @@ class JwtRequestFilterTest {
         String s = jwtRequestFilter.removePossibleSquareBrackets("Domain Users, LO-users, sillari_sillanvalvoja");
         assertEquals("Domain Users, LO-users, sillari_sillanvalvoja", s);
     }
+
+    @Test
+    void resolveYTunnus() {
+        JwtRequestFilter jwtRequestFilter = new JwtRequestFilter();
+        String s = jwtRequestFilter.resolvePossibleLOBusinessId("LOSV03575029");
+        assertEquals("0357502-9", s);
+
+        s = jwtRequestFilter.resolvePossibleLOBusinessId("LO03575029");
+        assertEquals("0357502-9", s);
+
+        s = jwtRequestFilter.resolvePossibleLOBusinessId("LOL03575029");
+        assertNull(s);
+
+        s = jwtRequestFilter.resolvePossibleLOBusinessId("XX03575029");
+        assertNull(s);
+        
+        s = jwtRequestFilter.resolvePossibleLOBusinessId("LO0357502");
+        assertNull(s);
+
+    }
 }
