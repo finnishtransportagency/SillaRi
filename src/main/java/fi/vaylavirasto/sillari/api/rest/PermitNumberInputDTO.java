f6a03b7ba4a36e9c727d769c583c39d711e5f58e
mustonenk <kalle.mustonen@cgi.com>
2022-10-31 14:03:52 +0200

    post getroutes

---
+++ src/main/java/fi/vaylavirasto/sillari/api/rest/PermitNumberInputDTO.java
@@ -0,0 +1,15 @@
+package fi.vaylavirasto.sillari.api.rest;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PermitNumberInputDTO {
+
+    public String permitNumber;
+
+
+}
