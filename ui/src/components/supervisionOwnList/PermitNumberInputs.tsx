48abada1d1eca806cba454a98d33be5ce2b5e22d
mustonenk <kalle.mustonen@cgi.com>
2022-11-01 10:50:34 +0200

    own list button styles

--- ui/src/components/supervisionOwnList/PermitNumberInputs.tsx
+++ ui/src/components/supervisionOwnList/PermitNumberInputs.tsx
@@ -6,7 +6,7 @@ import { getPermitRoutes } from "../../utils/areaContractorBackendData";
 import { useDispatch } from "react-redux";
 import OwnListPermitRouteType from "./OwnListPermitRouteType";
 import add from "../../theme/icons/add.svg";
-import arrowRight from "../../theme/icons/arrow-right.svg";
+import arrowRight from "../../theme/icons/arrow-right_white.svg";
 import { RootState, useTypedSelector } from "../../store/store";
 
 interface PermitNumberInputsProps {
@@ -85,7 +85,7 @@ const PermitNumberInputs = ({ permitRoutes, cancel, toNextPhase }: PermitNumberI
           <IonCol>
             <IonButton class="ion-button ion-float-right" onClick={getPermits}>
               {t("supervisionOwnList.addModal.permitNumberInput.continueButtonLabel")}
-              <IonIcon className="otherIcon" color="light" icon={arrowRight} />
+              <IonIcon className="otherIcon" icon={arrowRight} />
             </IonButton>
           </IonCol>
         </IonRow>
