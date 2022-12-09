b14fcccd706527a8624a7f8af67a287b23796901
mustonenk <Lncj6324Z>
2022-12-09 11:12:50 +0200

    rollback to old offline banner

--- ui/src/components/OfflineBanner.tsx
+++ ui/src/components/OfflineBanner.tsx
@@ -5,7 +5,6 @@ import { IonButton, IonIcon, IonItem, IonLabel } from "@ionic/react";
 import help from "../theme/icons/help_white.svg";
 import OfflineInfoModal from "./OfflineInfoModal";
 import "./OfflineBanner.css";
-import { RootState, useTypedSelector } from "../store/store";
 
 const OfflineBanner = (): JSX.Element => {
   const { t } = useTranslation();
@@ -13,22 +12,18 @@ const OfflineBanner = (): JSX.Element => {
   const [isOnline, setOnline] = useState<boolean>(onlineManager.isOnline());
   const [isOfflineInfoOpen, setOfflineInfoOpen] = useState<boolean>(false);
 
-  const {
-    networkStatus: { failedStatus = {} },
-  } = useTypedSelector((state: RootState) => state.rootReducer);
-
   useEffect(() => {
     onlineManager.subscribe(() => {
       setOnline(onlineManager.isOnline());
 
-      if (onlineManager.isOnline() && (!failedStatus.getUserData || failedStatus.getUserData < 400)) {
+      if (onlineManager.isOnline()) {
         setOfflineInfoOpen(false);
       }
     });
-  }, [failedStatus.getUserData]);
+  }, []);
 
   return (
-    <IonItem className={`offlineBanner ${isOnline && (!failedStatus.getUserData || failedStatus.getUserData < 400) ? "ion-hide" : ""}`} lines="none">
+    <IonItem className={`offlineBanner ${isOnline ? "ion-hide" : ""}`} lines="none">
       <IonLabel className="headingBoldText ion-text-center">{t("main.offline")}</IonLabel>
       <IonButton slot="end" className="ion-no-padding" size="default" fill="clear" onClick={() => setOfflineInfoOpen(true)}>
         <IonIcon slot="icon-only" icon={help} />
