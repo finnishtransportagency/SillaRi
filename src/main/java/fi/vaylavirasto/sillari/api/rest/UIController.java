63f8be9916214f9c0b09fc8b41dd5d9935584a43
mustonenk <kalle.mustonen@cgi.com>
2022-12-14 12:32:52 +0200

    login modal instead of tab, local login test code userdata

--- src/main/java/fi/vaylavirasto/sillari/api/rest/UIController.java
+++ src/main/java/fi/vaylavirasto/sillari/api/rest/UIController.java
@@ -227,12 +227,15 @@ public class UIController {
             responseBody.put("businessId", user.getBusinessId());
             responseBody.put("organization", user.getOrganization());
 
-
+            if(Math.random()<0.5) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
+            }
             return ResponseEntity.status(HttpStatus.OK).body(responseBody);
         } finally {
             serviceMetric.end();
         }
     }
+
     private static String buildNumber = "";
     private static String versionNumber = null;
     private static String version = null;
