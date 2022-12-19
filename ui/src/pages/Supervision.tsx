114a9a61e67acc436701d9c590cb001cbcc94146
mustonenk <kalle.mustonen@cgi.com>
2022-12-19 15:45:28 +0200

    set modified report from back end only once when opened from sending list

--- ui/src/pages/Supervision.tsx
+++ ui/src/pages/Supervision.tsx
@@ -260,7 +260,7 @@ const Supervision = (): JSX.Element => {
   }, [isLoading, supervision, modifiedReport, savedReport]);
 
   useEffect(() => {
-    console.log("hello: " );
+    console.log("hello: ");
     if (!isLoading && supervision) {
       if (supervisionOpenedFromSendingList && !modifiedReportResetted) {
         console.log("setModifiedReport undefined cause from send list");
